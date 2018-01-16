485c12b25e685ada0ae9e8f263c65cf4b5cc9cf5
Dan Bate <dan@wildfish.com>
2018-01-16 13:51:16 +0000

    added tests for comressor raising errors

--- tests/utils/test_compress.py
+++ tests/utils/test_compress.py
@@ -4,9 +4,10 @@ import zlib
 
 from hypothesis import given
 from hypothesis.strategies import binary
-from mock import patch
+from mock import patch, Mock
 
 from oasislmf.utils.compress import compress_data
+from oasislmf.utils.exceptions import OasisException
 
 MOCKED_CHUNK_SIZE = 100
 
@@ -39,3 +40,19 @@ class CompressData(TestCase):
         result = compress_data(front + overflow)
 
         self.assertEqual(expected, result)
+
+    def test_compress_raises_a_zlib_error___error_is_converted_to_oasis_error(self):
+        class FakeCompressor(object):
+            def compress(self, *args, **kwargs):
+                raise zlib.error()
+
+        with patch('zlib.compressobj', Mock(return_value=FakeCompressor())), self.assertRaises(OasisException):
+            compress_data(b'data')
+
+    def test_compress_raises_a_non_zlib_error___error_is_raised_without_converting(self):
+        class FakeCompressor(object):
+            def compress(self, *args, **kwargs):
+                raise ValueError()
+
+        with patch('zlib.compressobj', Mock(return_value=FakeCompressor())), self.assertRaises(ValueError):
+            compress_data(b'data')
