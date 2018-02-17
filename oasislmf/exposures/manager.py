a849f1a33c44bedf6601d10c61ca24c5581f02f0
Sandeep Murthy <smurthy@protonmail.ch>
2018-02-17 09:22:59 +0000

    Update exposure manager interface - remove files pipelinemethods

--- oasislmf/exposures/manager.py
+++ oasislmf/exposures/manager.py
@@ -51,69 +51,11 @@ class OasisExposuresManagerInterface(Interface):  # pragma: no cover
         """
         pass
 
-    @classmethod
-    def start_files_pipeline(cls, oasis_model=None, oasis_files_path=None, source_exposures_path=None):
-        """
-        Starts the exposure transforms pipeline for the given ``oasis_model``,
-        i.e. the generation of the canonical exposures files, keys file
-        and finally the Oasis files.
-
-        All the required resources must be provided either in the model object
-        resources dict or the ``kwargs`` dict.
-
-        It is up to the specific implementation of a manager of whether to
-        use the model object resources dict or additional optional arguments
-        in ``kwargs`` for this process.
-
-        In a standard implementation of the manager the call to
-        `start_files_pipeline` should trigger calls to the individual methods for
-        performing file transformations in a normal sequence, e.g.
-
-            `transform_source_to_canonical`
-            `transform_canonical_to_model`
-            `transform_model_to_keys`
-            `load_canonical_profile`
-            `generate_oasis_files`
-
-        and the generated files should be stored as attributes in the given
-        model object's transforms files pipeline.
-        """
-        pass
-
-    def clear_files_pipeline(self, oasis_model, **kwargs):
-        """
-        Clears the exposure transforms files pipeline for the given
-        ``oasis_model`` optionally using additional arguments in the ``kwargs``
-        dict.
-
-        All the required resources must be provided either in the model object
-        resources dict or the ``kwargs`` dict.
-
-        In the design of the exposure transform framework a model's files
-        pipeline is an object value in its resources dict with the key
-        ``transforms_files_pipeline`` and is thereby accessible with
-
-            `oasis_model.resources['transforms_files_pipeline']`
-
-        This returns an object of type
-
-            `exposure_transforms.OasisExposureTransformsFilesPipeline`
-
-        which stores the different files in the transformation stages for the
-        model as property attributes, e.g.
-
-            `oasis_model.resources['transforms_files_pipeline'].source_exposures_file`
-
-        A standard implementation could either assign a new object of this
-        type in the call to ``clear_files_pipeline``, or set some subset of the
-        file attributes of this pipelines object to null.
-        """
-        pass
 
     def transform_source_to_canonical(self, oasis_model, **kwargs):
         """
-        Transforms the source exposures/locations file for a given
-        ``oasis_model`` object to a canonical/standard Oasis format.
+        Transforms the source exposures/locations for a given ``oasis_model``
+        object to a canonical/standard Oasis format.
 
         All the required resources must be provided either in the model object
         resources dict or the ``kwargs`` dict.
@@ -129,8 +71,8 @@ class OasisExposuresManagerInterface(Interface):  # pragma: no cover
 
     def transform_canonical_to_model(self, oasis_model, **kwargs):
         """
-        Transforms the canonical exposures/locations file for a given
-        ``oasis_model`` object to a format understood by Oasis keys lookup
+        Transforms the canonical exposures/locations for a given ``oasis_model``
+        object to a format understood by Oasis keys lookup
         services.
 
         All the required resources must be provided either in the model object
