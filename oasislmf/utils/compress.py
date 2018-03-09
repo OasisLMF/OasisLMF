ba37fc88dd90fe4a83a86ab56a513ab367b52534
Sandeep Murthy <smurthy@protonmail.ch>
2018-03-09 12:52:07 +0000

    Refactor and rename `utils.csv.get_csv_rows_as_dicts` method

--- oasislmf/utils/compress.py
+++ oasislmf/utils/compress.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import zlib
 
 from .exceptions import OasisException
--- oasislmf/utils/conf.py
+++ oasislmf/utils/conf.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
     Utilities for running system or file-related commands, and other OS-related utilities.
 """
