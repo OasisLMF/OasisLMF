028a1d93edeed1bc763ff3c092dd4bc5c793f38c
Sandeep Murthy <smurthy@protonmail.ch>
2018-03-22 12:53:40 +0000

    Update MDK oasis files pipeline class - add keys error file path to pipeline

--- oasislmf/exposures/pipeline.py
+++ oasislmf/exposures/pipeline.py
@@ -9,35 +9,38 @@ class OasisFilesPipeline(object):
     def __init__(
         self,
         model_key=None,
-        source_exposures_path=None,
-        canonical_exposures_path=None,
-        model_exposures_path=None,
-        keys_file_path=None
+        source_exposures_file_path=None,
+        canonical_exposures_file_path=None,
+        model_exposures_file_path=None,
+        keys_file_path=None,
+        keys_error_file_path=None
     ):
         self._model_key = model_key
-        self._source_exposures_path = source_exposures_path
-        self._canonical_exposures_path = canonical_exposures_path
-        self._model_exposures_path = model_exposures_path
+        self._source_exposures_file_path = source_exposures_file_path
+        self._canonical_exposures_file_path = canonical_exposures_file_path
+        self._model_exposures_file_path = model_exposures_file_path
         self._keys_file_path = keys_file_path
+        self._keys_error_file_path = keys_error_file_path
 
-        self._items_path = None
-        self._coverages_path = None
-        self._gulsummaryxref_path = None
+        self._items_file_path = None
+        self._coverages_file_path = None
+        self._gulsummaryxref_file_path = None
 
         self._oasis_files = {
-            'items': self._items_path,
-            'coverages': self._coverages_path,
-            'gulsummaryxref': self._gulsummaryxref_path
+            'items': self._items_file_path,
+            'coverages': self._coverages_file_path,
+            'gulsummaryxref': self._gulsummaryxref_file_path
         }
 
-        self._file_attrib_names = (
-            'source_exposures_file',
-            'canonical_exposures_file',
-            'model_exposures_file',
-            'keys_file',
-            'items_file',
-            'coverages_file',
-            'gulsummaryxref_file',
+        self._file_paths = (
+            'source_exposures_file_path',
+            'canonical_exposures_file_path',
+            'model_exposures_file_path',
+            'keys_file_path',
+            'keys_error_file_path',
+            'items_file_path',
+            'coverages_file_path',
+            'gulsummaryxref_file_path',
         )
 
     def __str__(self):
@@ -59,118 +62,132 @@ class OasisFilesPipeline(object):
         return self._model_key
 
     @property
-    def source_exposures_path(self):
+    def source_exposures_file_path(self):
         """
-        Source exposures file property.
+        Source exposures file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._source_exposures_path
+        return self._source_exposures_file_path
 
-    @source_exposures_path.setter
-    def source_exposures_path(self, f):
-        self._source_exposures_path = f
+    @source_exposures_file_path.setter
+    def source_exposures_file_path(self, p):
+        self._source_exposures_file_path = p
 
     @property
-    def canonical_exposures_path(self):
+    def canonical_exposures_file_path(self):
         """
-        Canonical exposures file property.
+        Canonical exposures file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._canonical_exposures_path
+        return self._canonical_exposures_file_path
 
-    @canonical_exposures_path.setter
-    def canonical_exposures_path(self, f):
-        self._canonical_exposures_path = f
+    @canonical_exposures_file_path.setter
+    def canonical_exposures_file_path(self, p):
+        self._canonical_exposures_file_path = p
 
     @property
-    def model_exposures_path(self):
+    def model_exposures_file_path(self):
         """
-        Model exposures file property.
+        Model exposures file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._model_exposures_path
+        return self._model_exposures_file_path
 
-    @model_exposures_path.setter
-    def model_exposures_path(self, f):
-        self._model_exposures_path = f
+    @model_exposures_file_path.setter
+    def model_exposures_file_path(self, p):
+        self._model_exposures_file_path = p
 
     @property
     def keys_file_path(self):
         """
-        Oasis keys file property.
+        Oasis keys file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
         return self._keys_file_path
 
     @keys_file_path.setter
-    def keys_file_path(self, f):
-        self._keys_file_path = f
+    def keys_file_path(self, p):
+        self._keys_file_path = p
+
+    @property
+    def keys_error_file_path(self):
+        """
+        Oasis keys error file path property.
+
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
+        """
+        return self._keys_file_path
+
+    @keys_error_file_path.setter
+    def keys_error_file_path(self, p):
+        self._keys_error_file_path = p
 
     @property
     def items_file_path(self):
         """
-        Oasis items file property.
+        Oasis items file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._items_path
+        return self._items_file_path
 
     @items_file_path.setter
-    def items_file_path(self, f):
-        self._items_path = self.oasis_files['items'] = f
+    def items_file_path(self, p):
+        self._items_file_path = self.oasis_files['items'] = p
 
     @property
-    def coverages_path(self):
+    def coverages_file_path(self):
         """
-        Oasis coverages file property.
+        Oasis coverages file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._coverages_path
+        return self._coverages_file_path
 
-    @coverages_path.setter
-    def coverages_path(self, f):
-        self._coverages_path = self.oasis_files['coverages'] = f
+    @coverages_file_path.setter
+    def coverages_file_path(self, p):
+        self._coverages_file_path = self.oasis_files['coverages'] = p
 
     @property
-    def gulsummaryxref_path(self):
+    def gulsummaryxref_file_path(self):
         """
-        GUL summary file property.
+        GUL summary file path property.
 
-            :getter: Gets the actual file object
-            :setter: Sets the file to the specified file object
+            :getter: Gets the file path
+            :setter: Sets the current file path to the specified file path
         """
-        return self._gulsummaryxref_path
+        return self._gulsummaryxref_file_path
 
-    @gulsummaryxref_path.setter
-    def gulsummaryxref_path(self, f):
-        self._gulsummaryxref_path = self.oasis_files['gulsummaryxref'] = f
+    @gulsummaryxref_file_path.setter
+    def gulsummaryxref_file_path(self, p):
+        self._gulsummaryxref_file_path = self.oasis_files['gulsummaryxref'] = p
 
     @property
     def oasis_files(self):
         """
         Oasis files set property - getter only.
 
-            :getter: Gets the complete set of generated Oasis files, including
-                     ``items.csv``, ``coverages.csv``, `gulsummaryxref.csv`.
+            :getter: Gets the complete set of paths of the generated Oasis
+            files, including  ``items.csv``, ``coverages.csv``, `gulsummaryxref.csv`.
         """
         return self._oasis_files
 
     def clear(self):
         """
-        Clears all file attributes in the pipeline.
+        Clears all file path attributes in the pipeline.
         """
         map(
-            lambda f: setattr(self, f, None),
-            self._file_attrib_names
+            lambda p: setattr(self, p, None),
+            self._file_paths
         )
