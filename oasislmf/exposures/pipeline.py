fbf3c52b85caaf9179165ee4ffc1bd6a016e25ad
Sandeep Murthy <smurthy@protonmail.ch>
2018-03-27 16:47:19 +0100

    Fix rebase conflicts - add FM files properties to Oasis files pipeline + add FM files generation method stub in exposure manager and integrate FM files generation method into Oasis files generation method + add FM files generation flag to Oasis files generation and Oasis files pipeline start methods in exposure manager + add FM files generation flag to Oasis files generation command class

--- oasislmf/exposures/pipeline.py
+++ oasislmf/exposures/pipeline.py
@@ -26,22 +26,27 @@ class OasisFilesPipeline(object):
         self._coverages_file_path = None
         self._gulsummaryxref_file_path = None
 
-        self._oasis_files = {
+        self._fm_policytc_file_path = None
+        self._fm_profile_file_path = None
+        self._fm_programme_file_path = None
+        self._fm_xref_file_path = None
+        self._fmsummaryxref_file_path = None
+
+        self._exposure_files = {
             'items': self._items_file_path,
             'coverages': self._coverages_file_path,
             'gulsummaryxref': self._gulsummaryxref_file_path
         }
 
-        self._file_paths = (
-            'source_exposures_file_path',
-            'canonical_exposures_file_path',
-            'model_exposures_file_path',
-            'keys_file_path',
-            'keys_error_file_path',
-            'items_file_path',
-            'coverages_file_path',
-            'gulsummaryxref_file_path',
-        )
+        self._fm_files = {
+            'fm_policytc': self._fm_policytc_file_path,
+            'fm_profile': self._fm_profile_file_path,
+            'fm_programme': self._fm_programme_file_path,
+            'fm_xref': self._fm_xref_file_path,
+            'fmsummaryxref': self._fmsummaryxref_file_path
+        }
+
+        self._oasis_files = {k:v for k, v in self._exposure_files.items() + self._fm_files.items()}
 
     def __str__(self):
         return '{}: {}'.format(self.__repr__(), self.model_key)
@@ -143,7 +148,7 @@ class OasisFilesPipeline(object):
 
     @items_file_path.setter
     def items_file_path(self, p):
-        self._items_file_path = self.oasis_files['items'] = p
+        self._items_file_path = self.exposure_files['items'] = self.oasis_files['items'] = p
 
     @property
     def coverages_file_path(self):
@@ -157,7 +162,7 @@ class OasisFilesPipeline(object):
 
     @coverages_file_path.setter
     def coverages_file_path(self, p):
-        self._coverages_file_path = self.oasis_files['coverages'] = p
+        self._coverages_file_path = self.exposure_files['coverages'] = self.oasis_files['items'] = p
 
     @property
     def gulsummaryxref_file_path(self):
@@ -171,23 +176,124 @@ class OasisFilesPipeline(object):
 
     @gulsummaryxref_file_path.setter
     def gulsummaryxref_file_path(self, p):
-        self._gulsummaryxref_file_path = self.oasis_files['gulsummaryxref'] = p
+        self._gulsummaryxref_file_path = self.exposure_files['gulsummaryxref'] = self.oasis_files['items'] = p
+
+    @property
+    def fm_policytc_file_path(self):
+        """
+        FM policy T & C file property.
+
+            :getter: Gets the actual file object
+            :setter: Sets the file to the specified file object
+        """
+        return self._fm_policytc_file_path
+
+    @fm_policytc_file_path.setter
+    def fm_policytc_file_path(self, f):
+        self._fm_policytc_file_path = self.fm_files['fm_policytc'] = self.oasis_files['items'] = f
+
+    @property
+    def fm_profile_file_path(self):
+        """
+        FM profile file property.
+
+            :getter: Gets the actual file object
+            :setter: Sets the file to the specified file object
+        """
+        return self._fm_profile_file_path
+
+    @fm_profile_file_path.setter
+    def fm_profile_file_path(self, f):
+        self._fm_profile_file_path = self.fm_files['fm_profile'] = self.oasis_files['items'] = f
+
+    @property
+    def fm_programme_file_path(self):
+        """
+        FM programme file property.
+
+            :getter: Gets the actual file object
+            :setter: Sets the file to the specified file object
+        """
+        return self._fm_programme_file_path
+
+    @fm_programme_file_path.setter
+    def fm_programme_file_path(self, f):
+        self._fm_programme_file_path = self.fm_files['fm_programme'] = self.oasis_files['items'] = f
+
+    @property
+    def fm_xref_file_path(self):
+        """
+        FM xref file property.
+
+            :getter: Gets the actual file object
+            :setter: Sets the file to the specified file object
+        """
+        return self._fm_xref_file_path
+
+    @fm_xref_file_path.setter
+    def fm_xref_file_path(self, f):
+        self._fm_xref_file_path = self.fm_files['fm_xref'] = self.oasis_files['items'] = f
+
+    @property
+    def fmsummaryxref_file_path(self):
+        """
+        FM fmsummaryxref file property.
+
+            :getter: Gets the actual file object
+            :setter: Sets the file to the specified file object
+        """
+        return self._fmfmsummaryxref_file_path
+
+    @fmsummaryxref_file_path.setter
+    def fmsummaryxref_file_path(self, f):
+        self._fmsummaryxref_file_path = self.fm_files['fmsummaryxref'] = self.oasis_files['items'] = f
+
+
+    @property
+    def exposure_files(self):
+        """
+        Oasis exposure files set property - getter only.
+
+            :getter: Gets the complete set of paths of the generated Oasis
+            exposure files, including  ``items.csv``, ``coverages.csv``, `gulsummaryxref.csv`.
+        """
+        return self._exposure_files
+
+    @property
+    def fm_files(self):
+        """
+        Oasis FM files set property - getter only.
+
+            :getter: Gets the complete set of generated FM files, including
+                     ``fm_policytc.csv``, ``fm_profile.csv``, `fm_programme.csv`,
+                     ``fm_xref.csv``, ``fmsummaryxref.csv``.
+        """
+        return self._fm_files
 
     @property
     def oasis_files(self):
         """
-        Oasis files set property - getter only.
+        Oasis exposure + FM files set property - getter only.
 
-            :getter: Gets the complete set of paths of the generated Oasis
-            files, including  ``items.csv``, ``coverages.csv``, `gulsummaryxref.csv`.
+            :getter: Gets the complete set of generated exposure + FM files,
+                     including ``items.csv`, ``coverages.csv``,
+                     ``gulsummaryxref.csv``, ``fm_policytc.csv``,
+                     ``fm_profile.csv``, `fm_programme.csv`,
+                     ``fm_xref.csv``, ``fmsummaryxref.csv``.
         """
         return self._oasis_files
 
-    def clear(self):
+
+    def clear(self, exposure_only=False, fm_only=False):
         """
         Clears all file path attributes in the pipeline.
         """
+        if not (exposure_only or fm_only):
+            filenames = self.oasis_files.keys()
+        else:
+            filenames = self.exposure_files.keys() if exposure_only else self.fm_files.keys()
+
         map(
-            lambda p: setattr(self, p, None),
-            self._file_paths
+            lambda p: setattr(self, '{}_file_path'.format(p), None),
+            filenames
         )
