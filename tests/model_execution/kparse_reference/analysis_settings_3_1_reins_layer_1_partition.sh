987055c4d8424cb4f0fb9732388d0964e33e1686
Mark Pinkerton <mark.pinkerton@oasislnf.org>
2018-08-14 13:22:22 +0100

    Added reinsurance test

---
+++ tests/model_execution/kparse_reference/analysis_settings_3_1_reins_layer_1_partition.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+rm -R -f output/*
+rm -R -f fifo/*
+rm -R -f work/*
+
+mkdir work/kat
+mkfifo fifo/gul_P1
+
+mkfifo fifo/gul_S1_summary_P1
+mkfifo fifo/gul_S1_summaryeltcalc_P1
+mkfifo fifo/gul_S1_eltcalc_P1
+mkfifo fifo/gul_S1_summarysummarycalc_P1
+mkfifo fifo/gul_S1_summarycalc_P1
+mkfifo fifo/gul_S1_summarypltcalc_P1
+mkfifo fifo/gul_S1_pltcalc_P1
+mkfifo fifo/gul_S1_summaryaalcalc_P1
+
+mkdir work/gul_S1_aalcalc
+mkfifo fifo/il_P1
+
+mkfifo fifo/il_S1_summary_P1
+mkfifo fifo/il_S1_summaryeltcalc_P1
+mkfifo fifo/il_S1_eltcalc_P1
+mkfifo fifo/il_S1_summarysummarycalc_P1
+mkfifo fifo/il_S1_summarycalc_P1
+mkfifo fifo/il_S1_summarypltcalc_P1
+mkfifo fifo/il_S1_pltcalc_P1
+mkfifo fifo/il_S1_summaryaalcalc_P1
+
+mkdir work/il_S1_aalcalc
+mkfifo fifo/ri_P1
+
+mkfifo fifo/ri_S1_summary_P1
+mkfifo fifo/ri_S1_summaryeltcalc_P1
+mkfifo fifo/ri_S1_eltcalc_P1
+mkfifo fifo/ri_S1_summarysummarycalc_P1
+mkfifo fifo/ri_S1_summarycalc_P1
+mkfifo fifo/ri_S1_summarypltcalc_P1
+mkfifo fifo/ri_S1_pltcalc_P1
+mkfifo fifo/ri_S1_summaryaalcalc_P1
+
+mkdir work/ri_S1_aalcalc
+
+
+# --- Do reinsurance loss computes ---
+
+eltcalc < fifo/ri_S1_summaryeltcalc_P1 > work/kat/ri_S1_eltcalc_P1 & pid1=$!
+summarycalctocsv < fifo/ri_S1_summarysummarycalc_P1 > work/kat/ri_S1_summarycalc_P1 & pid2=$!
+pltcalc < fifo/ri_S1_summarypltcalc_P1 > work/kat/ri_S1_pltcalc_P1 & pid3=$!
+aalcalc < fifo/ri_S1_summaryaalcalc_P1 > work/ri_S1_aalcalc/P1.bin & pid4=$!
+
+tee < fifo/ri_S1_summary_P1 fifo/ri_S1_summaryeltcalc_P1 fifo/ri_S1_summarypltcalc_P1 fifo/ri_S1_summarysummarycalc_P1 fifo/ri_S1_summaryaalcalc_P1 > /dev/null & pid5=$!
+summarycalc -g -1 fifo/ri_S1_summary_P1 < fifo/ri_P1 &
+
+# --- Do insured loss computes ---
+
+eltcalc < fifo/il_S1_summaryeltcalc_P1 > work/kat/il_S1_eltcalc_P1 & pid6=$!
+summarycalctocsv < fifo/il_S1_summarysummarycalc_P1 > work/kat/il_S1_summarycalc_P1 & pid7=$!
+pltcalc < fifo/il_S1_summarypltcalc_P1 > work/kat/il_S1_pltcalc_P1 & pid8=$!
+aalcalc < fifo/il_S1_summaryaalcalc_P1 > work/il_S1_aalcalc/P1.bin & pid9=$!
+
+tee < fifo/il_S1_summary_P1 fifo/il_S1_summaryeltcalc_P1 fifo/il_S1_summarypltcalc_P1 fifo/il_S1_summarysummarycalc_P1 fifo/il_S1_summaryaalcalc_P1 > /dev/null & pid10=$!
+summarycalc -f -1 fifo/il_S1_summary_P1 < fifo/il_P1 &
+
+# --- Do ground up loss computes ---
+
+eltcalc < fifo/gul_S1_summaryeltcalc_P1 > work/kat/gul_S1_eltcalc_P1 & pid11=$!
+summarycalctocsv < fifo/gul_S1_summarysummarycalc_P1 > work/kat/gul_S1_summarycalc_P1 & pid12=$!
+pltcalc < fifo/gul_S1_summarypltcalc_P1 > work/kat/gul_S1_pltcalc_P1 & pid13=$!
+aalcalc < fifo/gul_S1_summaryaalcalc_P1 > work/gul_S1_aalcalc/P1.bin & pid14=$!
+
+tee < fifo/gul_S1_summary_P1 fifo/gul_S1_summaryeltcalc_P1 fifo/gul_S1_summarypltcalc_P1 fifo/gul_S1_summarysummarycalc_P1 fifo/gul_S1_summaryaalcalc_P1 > /dev/null & pid15=$!
+summarycalc -g -1 fifo/gul_S1_summary_P1 < fifo/gul_P1 &
+
+eve 1 1 | getmodel | gulcalc -S0 -L0 -r -c fifo/gul_P1 -i - | fmcalc > fifo/il_P1  &
+
+wait $pid1 $pid2 $pid3 $pid4 $pid5 $pid6 $pid7 $pid8 $pid9 $pid10 $pid11 $pid12 $pid13 $pid14 $pid15
+
+
+# --- Do reinsurance loss kats ---
+
+kat work/kat/ri_S1_eltcalc_P1 > output/ri_S1_eltcalc.csv & kpid1=$!
+kat work/kat/ri_S1_pltcalc_P1 > output/ri_S1_pltcalc.csv & kpid2=$!
+kat work/kat/ri_S1_summarycalc_P1 > output/ri_S1_summarycalc.csv & kpid3=$!
+
+# --- Do insured loss kats ---
+
+kat work/kat/il_S1_eltcalc_P1 > output/il_S1_eltcalc.csv & kpid4=$!
+kat work/kat/il_S1_pltcalc_P1 > output/il_S1_pltcalc.csv & kpid5=$!
+kat work/kat/il_S1_summarycalc_P1 > output/il_S1_summarycalc.csv & kpid6=$!
+
+# --- Do ground up loss kats ---
+
+kat work/kat/gul_S1_eltcalc_P1 > output/gul_S1_eltcalc.csv & kpid7=$!
+kat work/kat/gul_S1_pltcalc_P1 > output/gul_S1_pltcalc.csv & kpid8=$!
+kat work/kat/gul_S1_summarycalc_P1 > output/gul_S1_summarycalc.csv & kpid9=$!
+wait $kpid1 $kpid2 $kpid3 $kpid4 $kpid5 $kpid6 $kpid7 $kpid8 $kpid9
+
+
+aalsummary -Kil_S1_aalcalc > output/il_S1_aalcalc.csv & apid1=$!
+aalsummary -Kgul_S1_aalcalc > output/gul_S1_aalcalc.csv & apid2=$!
+wait $apid1 $apid2
+
+rm fifo/gul_P1
+
+rm fifo/gul_S1_summary_P1
+rm fifo/gul_S1_summaryeltcalc_P1
+rm fifo/gul_S1_eltcalc_P1
+rm fifo/gul_S1_summarysummarycalc_P1
+rm fifo/gul_S1_summarycalc_P1
+rm fifo/gul_S1_summarypltcalc_P1
+rm fifo/gul_S1_pltcalc_P1
+rm fifo/gul_S1_summaryaalcalc_P1
+
+rm -rf work/kat
+rm work/gul_S1_aalcalc/*
+rmdir work/gul_S1_aalcalc
+
+rm fifo/il_P1
+
+rm fifo/il_S1_summary_P1
+rm fifo/il_S1_summaryeltcalc_P1
+rm fifo/il_S1_eltcalc_P1
+rm fifo/il_S1_summarysummarycalc_P1
+rm fifo/il_S1_summarycalc_P1
+rm fifo/il_S1_summarypltcalc_P1
+rm fifo/il_S1_pltcalc_P1
+rm fifo/il_S1_summaryaalcalc_P1
+
+rm -rf work/kat
+rm work/il_S1_aalcalc/*
+rmdir work/il_S1_aalcalc
