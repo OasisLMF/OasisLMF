4a8339ac9bf8f6d3725b08934e3590b64b237a5a
Mark Pinkerton <mark.pinkerton@oasislnf.org>
2018-08-20 10:26:33 +0100

    Added file generation utilities.

--- oasislmf/exposures/reinsurance_layer.py
+++ oasislmf/exposures/reinsurance_layer.py
@@ -62,6 +62,104 @@ def valid_links(df_src, column_name, df_dest):
     return df_dest.isin({column_name: src_values}).all()
 
 
+def load_oed_dfs(oed_dir, show_all=False):
+    """
+    Load OED data files.
+    """
+
+    do_reinsurance = True
+    if oed_dir is not None:
+        if not os.path.exists(oed_dir):
+            print("Path does not exist: {}".format(oed_dir))
+            exit(1)
+        # Account file
+        oed_account_file = os.path.join(oed_dir, "account.csv")
+        if not os.path.exists(oed_account_file):
+            print("Path does not exist: {}".format(oed_account_file))
+            exit(1)
+        account_df = pd.read_csv(oed_account_file)
+
+        # Location file
+        oed_location_file = os.path.join(oed_dir, "location.csv")
+        if not os.path.exists(oed_location_file):
+            print("Path does not exist: {}".format(oed_location_file))
+            exit(1)
+        location_df = pd.read_csv(oed_location_file)
+
+        # RI files
+        oed_ri_info_file = os.path.join(oed_dir, "ri_info.csv")
+        oed_ri_scope_file = os.path.join(oed_dir, "ri_scope.csv")
+        oed_ri_info_file_exists = os.path.exists(oed_ri_info_file)
+        oed_ri_scope_file_exists = os.path.exists(oed_ri_scope_file)
+
+        if not oed_ri_info_file_exists and not oed_ri_scope_file_exists:
+            ri_info_df = None
+            ri_scope_df = None
+            do_reinsurance = False
+        elif oed_ri_info_file_exists and oed_ri_scope_file_exists:
+            ri_info_df = pd.read_csv(oed_ri_info_file)
+            ri_scope_df = pd.read_csv(oed_ri_scope_file)
+        else:
+            print("Both reinsurance files must exist: {} {}".format(
+                oed_ri_info_file, oed_ri_scope_file))
+        if not show_all:
+            account_df = account_df[common.OED_ACCOUNT_FIELDS].copy()
+            location_df = location_df[common.OED_LOCATION_FIELDS].copy()
+            if do_reinsurance:
+                ri_info_df = ri_info_df[common.OED_REINS_INFO_FIELDS].copy()
+                ri_scope_df = ri_scope_df[common.OED_REINS_SCOPE_FIELDS].copy()
+    return (account_df, location_df, ri_info_df, ri_scope_df, do_reinsurance)
+
+
+def generate_files_for_reinsurance_risk_level(
+        inuring_priority,
+        account_df,
+        location_df,
+        items,
+        coverages,
+        fm_xrefs,
+        xref_descriptions,
+        ri_info_df,
+        ri_scope_df,
+        previous_inuring_priority,
+        previous_risk_level,
+        risk_level,
+        reinsurance_index,
+        direct_oasis_files_dir):
+    """
+    Generate files for a reinsurance risk level.
+    """
+    reins_numbers_1 = ri_info_df[
+        ri_info_df['InuringPriority'] == inuring_priority].ReinsNumber
+    if reins_numbers_1.empty:
+        return None
+    reins_numbers_2 = ri_scope_df[
+        ri_scope_df.isin({"ReinsNumber": reins_numbers_1.tolist()}).ReinsNumber &
+        (ri_scope_df.RiskLevel == risk_level)].ReinsNumber
+    if reins_numbers_2.empty:
+        return None
+
+    ri_info_inuring_priority_df = ri_info_df[ri_info_df.isin(
+        {"ReinsNumber": reins_numbers_2.tolist()}).ReinsNumber]
+    output_name = "ri_{}_{}".format(inuring_priority, risk_level)
+    reinsurance_layer = ReinsuranceLayer(
+        name=output_name,
+        ri_info=ri_info_inuring_priority_df,
+        ri_scope=ri_scope_df,
+        accounts=account_df,
+        locations=location_df,
+        items=items,
+        coverages=coverages,
+        fm_xrefs=fm_xrefs,
+        xref_descriptions=xref_descriptions,
+        risk_level=risk_level
+    )
+
+    reinsurance_layer.generate_oasis_structures()
+    output_dir = os.path.join(direct_oasis_files_dir, "RI_{}".format(reinsurance_index))
+    reinsurance_layer.write_oasis_files(output_dir)
+
+
 def validate_reinsurance_structures(account_df, location_df, ri_info_df, ri_scope_df):
     '''
     Validate OED resinurance structure before running calculations.
