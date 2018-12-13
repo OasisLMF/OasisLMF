a1296b6f87df18198608c8e0fa7f16909f3fcc7d
Sandeep Murthy <smurthy@protonmail.ch>
2018-12-13 09:23:03 +0000

    Update loss script - add parsing step

--- oasislmf/validation/deterministic-loss.py
+++ oasislmf/validation/deterministic-loss.py
@@ -2,3 +2,22 @@
 """
 Deterministic loss validation
 """
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Validate deterministic losses')
+    parser.add_argument(
+        '-n', '--output-dir', metavar='N', type=str, required=True,
+        help='The output directory'
+    parser.add_argument(
+        '-o', '--input-dir', metavar='N', type=str, default=None, required=False,
+        help='The input directory containing the set of OED exposure + accounts data + other assets required for MDK Oasis files generation')
+    parser.add_argument(
+        '-l', '--loss-factor', metavar='N', type=float, default=1.0,
+        help='The loss factor to apply to TIVs.')
+    parser.add_argument(
+       '-d', '--debug', action='store', default=None,
+       help='Store Debugging Logs under ./logs')
+
+    args = parser.parse_args()
