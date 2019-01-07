09f027f9d202a2c4426c7257788296814551a9c5
sambles <sambles@users.noreply.github.com>
2019-01-07 15:27:59 +0000

    Increase open files limit for testing

--- runtests.sh
+++ runtests.sh
@@ -12,5 +12,5 @@ else
     DOCKER_TAG=$1
 fi
 
-docker build -f docker/Dockerfile.oasislmf_tester -t oasislmf-tester:$DOCKER_TAG .
-docker run -v $LOG_OUTPUT:/var/log/oasis -v $TAR_OUTPUT:/tmp/output oasislmf-tester:$DOCKER_TAG
+docker build -f docker/Dockerfile.oasislmf_tester -t oasislmf-tester .
+docker run  --ulimit nofile=8192:8192 -v $LOG_OUTPUT:/var/log/oasis -v $TAR_OUTPUT:/tmp/output oasislmf-tester:$DOCKER_TAG
