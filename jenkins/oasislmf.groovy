800ff13d6ae62427687f44689df3824686a791e2
Sam Gamble <hexadessa@gmail.com>
2019-03-12 16:35:34 +0000

    Allow publish from hotfix/ branches

--- jenkins/oasislmf.groovy
+++ jenkins/oasislmf.groovy
@@ -64,7 +64,7 @@ node {
     env.PIPELINE_LOAD =  script_dir + source_sh             // required for pipeline.sh calls
     sh 'env'
 
-    if (params.PUBLISH && ! source_branch.matches("release/(.*)") ){
+    if (params.PUBLISH && ! ( source_branch.matches("release/(.*)") || source_branch.matches("hotfix/(.*)")) ){
         // fail fast, only branches named `release/*` are valid for publish
         sh "echo `Publish Only allowed on a release/* branch`"
         sh "exit 1"
