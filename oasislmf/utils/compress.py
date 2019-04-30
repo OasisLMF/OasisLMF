0fe1cc1f1bf723b1a8bc8dd8be6331e7be624200
Sandeep Murthy <smurthy@protonmail.ch>
2019-04-30 16:11:36 +0100

    Update (zlib) compression utils (adapt to Python 3) + add decompression method and update tests

--- oasislmf/utils/compress.py
+++ oasislmf/utils/compress.py
@@ -1,6 +1,7 @@
 __all__ = [
-    'compress_data',
-    'CHUNK_SIZE'
+    'compress_string',
+    'decompress_string',
+    'CHUNK_SIZE',
 ]
 
 import zlib
@@ -11,24 +12,30 @@ from .exceptions import OasisException
 CHUNK_SIZE = 5 * 10 ** 8  # 500 Mb
 
 
-def compress_data(s):
+def compress_string(st: str) -> bytes:
     """
-    Compress large data strings.
+    Compresses strings using the zlib library.
 
     Adapted from a StackOverflow.com solution by Dmitry Skryabin
 
         https://stackoverflow.com/a/36056646/7556955
 
     with a modification to set block/chunk size to 500 Mb (5 x 10^8 bytes).
-    """
 
+    :param s: Input string to be compressed
+    :type s: str
+
+    :return: Compressed string as bytes
+    :rtype: bytes
+    """
+    _st = ''.join(st).encode('utf-8')
     compressed = b''
     begin = 0
     compressor = zlib.compressobj()
 
     try:
-        while begin < len(s):
-            compressed += compressor.compress(s[begin:begin + CHUNK_SIZE])
+        while begin < len(_st):
+            compressed += compressor.compress(_st[begin:begin + CHUNK_SIZE])
             begin += CHUNK_SIZE
 
         compressed += compressor.flush()
@@ -36,3 +43,18 @@ def compress_data(s):
         raise OasisException from e
 
     return compressed
+
+
+def decompress_string(bt: bytes) -> str:
+    """
+    Decompresses zlib-compressed strings
+
+    :param bt: zlib-compressed string
+    :type bt: bytes
+
+    :return: Decompressed (Unicode) string
+    :rtype: str
+    """
+    decompressor = zlib.decompressobj()
+
+    return decompressor.decompress(bt).decode('utf-8')
