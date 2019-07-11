d512dff9bceff73e0d5729b1630812c5bf8622e1
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2019-07-11 15:49:36 +0100

    Initial commit; testing damagebintobin

---
+++ oasislmf/model_testing/validation.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+INPUT_FILES = ['damage_bin_dict': {'name': 'damage_bin_dict', 'coversion_tool': 'damagebintobin'}]
+
+def csv_to_bin(model_data_fp):
+
+    model_data_dir = os.path.abspath(model_data_fp)
+
+    for input_file in INPUT_FILES.values():
+        conversion_tool = input_file['conversion_tool']
+        input_file_path = os.path.join(
+            model_data_dir,
+            '{}.csv'.format(input_file['name'])
+        )
+
+        output_file_path = os.path.join(
+            model_data_dir,
+            '{}.bin'.format(input_file['name'])
+        )
+
+        cmd_str = "{} < {} > {}".format(conversion_tool, input_file_path, output_file_path)
+
+        try:
+            subprocess.check_call(cmd_str, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            raise OasisException from e
