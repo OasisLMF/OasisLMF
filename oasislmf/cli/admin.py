9777c4944d4aca591480e6f0a8b25c8d540f6ae9
Sandeep Murthy <smurthy@protonmail.ch>
2019-07-19 12:55:53 +0100

    Update complex model creation command

--- oasislmf/cli/admin.py
+++ oasislmf/cli/admin.py
@@ -70,8 +70,7 @@ class CreateSimpleModelCmd(OasisBaseCommand):
 
     def action(self, args):
         """
-        Generates and writes an Rtree file index of peril area IDs (area peril IDs)
-        and area polygon bounds from a peril areas (area peril) file.
+        Command action
 
         :param args: The arguments from the command line
         :type args: Namespace
@@ -125,7 +124,7 @@ class CreateSimpleModelCmd(OasisBaseCommand):
 class CreateComplexModelCmd(OasisBaseCommand):
     """
     Creates a local Git repository for a "complex model" (using the
-    ``cookiecutter`` package) on a "complex model" repository template
+    ``cookiecutter`` package) on a "simple model" repository template
     on GitHub)
     """
     formatter_class = RawDescriptionHelpFormatter
@@ -139,18 +138,79 @@ class CreateComplexModelCmd(OasisBaseCommand):
         """
         super(self.__class__, self).add_args(parser)
 
-        # parser.add_argument(
-        #)
+        parser.add_argument(
+            '-p', '--preset-cookiecutter-json', default=None, required=False, help='Cookiecutter JSON file path with all options provided in the file'
+        )
+        parser.add_argument(
+            '-i', '--no-input', default=None, help='Do not prompt for parameters and only use cookiecutter.json file content', action='store_true'
+        )
+        parser.add_argument(
+            '-r', '--replay', default=None, help='Do not prompt for parameters and only use information entered previously', action='store_true'
+        )
+        parser.add_argument(
+            '-f', '--overwrite-if-exists', default=None, help='Overwrite the contents of any preexisting project directory of the same name', action='store_true'
+        )
+        parser.add_argument(
+            '-o', '--output-dir', default=None, required=False, help='Where to generate the project'
+        )
+        parser.add_argument(
+            '-b', '--verbose-mode', default=None, required=False, help='Run cookiecutter in verbose mode', action='store_true'
+        )
+        parser.add_argument(
+            '-c', '--cookiecutter-version', default=None, required=False, help='Cookiecutter version', action='store_true'
+        )
 
     def action(self, args):
         """
-        Generates and writes an Rtree file index of peril area IDs (area peril IDs)
-        and area polygon bounds from a peril areas (area peril) file.
+        Command action
 
         :param args: The arguments from the command line
         :type args: Namespace
         """
         inputs = InputValues(args)
+
+        preset_cookiecutter_json = as_path(
+            inputs.get('preset_cookiecutter_json', required=False, is_path=True), label='Preset cookiecutter JSON', is_dir=False, preexists=True
+        )
+
+        no_prompt = inputs.get('no_input', default=False, required=False)
+
+        replay = inputs.get('replay', default=False, required=False)
+
+        overwrite = inputs.get('overwrite_if_exists', default=True, required=False)
+
+        target_dir = as_path(
+            inputs.get('output_dir', default=os.path.join(os.getcwd(), 'cookiecutter-run'), required=False, is_path=True), label='Target directory', is_dir=True, preexists=False
+        )
+
+        verbose = inputs.get('verbose_mode', default=False, required=False)
+
+        cookiecutter_version = inputs.get('cookiecutter_version', default=False, required=False)
+
+        def run_cmd(cmd_str):
+            run(cmd_str.split(), check=True)
+
+        cmd_str = 'cookiecutter'
+
+        if not cookiecutter_version:
+            os.chdir(STATIC_DATA_FP) if not preset_cookiecutter_json else os.chdir(os.path.dirname(preset_cookiecutter_json))
+            cmd_str += ''.join([ 
+                (' --no-input' if no_prompt or preset_cookiecutter_json else ''), 
+                (' --replay' if replay else ''), 
+                (' --overwrite-if-exists' if overwrite else ''), 
+                (' --output-dir {}'.format(target_dir) if target_dir else ''), 
+                (' --verbose ' if verbose else ' '), 
+                'git+ssh://git@github.com/OasisLMF/CookiecutterOasisComplexModel' 
+            ])
+        else:
+            cmd_str += ' -V'
+
+        self.logger.info('\nRunning cookiecutter command: {}\n'.format(cmd_str))
+
+        try:
+            run_cmd(cmd_str)
+        except CalledProcessError as e:
+            self.logger.error(e)
 
 class AdminCmd(OasisBaseCommand):
     """
