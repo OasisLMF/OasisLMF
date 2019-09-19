b7c706c7b2e59d8bf2dc0405c4630133982fd3d4
hchagani-oasislmf <47981469+hchagani-oasislmf@users.noreply.github.com>
2019-09-19 15:22:55 +0100

    Verifying model files with MDK commands (#397)
    * Initial commit; testing damagebintobin
    
    * Imported additional library
    
    * Change type list to dict
    
    * Fix syntax error + add docstring + add comment + alter layout
    
    * Added model-validation sub-command
    
    * Initial commit
    
    * Added list of public objects
    
    * Fix typo
    
    * Fix typo
    
    * Fix syntax
    
    * Added docstrings + changed variable passed to method
    
    * Use ktools validation executables
    
    * Fix broken method import
    
    * Add footprint file validation
    
    * Initial commit
    
    * Initial commit
    
    * Remove unused library
    
    * Added basic logging info
    
    * Add vulnerability file validation and cross checks
    
    * Correct cross checks arguments
    
    * Correct footprint file flag

--- oasislmf/cli/test.py
+++ oasislmf/cli/test.py
@@ -4,10 +4,17 @@ from argparsetree import BaseCommand
 
 from .. import __version__
 from ..model_preparation.lookup import OasisLookupFactory as olf
+from ..model_testing.validation import csv_validity_test
 from ..utils.conf import replace_in_file
 
-from ..utils.path import PathCleaner
-from .base import OasisBaseCommand
+from ..utils.path import (
+    as_path,
+    PathCleaner,
+)
+from .base import (
+    InputValues,
+    OasisBaseCommand,
+)
 
 
 class GenerateModelTesterDockerFileCmd(OasisBaseCommand):
@@ -84,6 +91,39 @@ class GenerateModelTesterDockerFileCmd(OasisBaseCommand):
         return 0
 
 
+class ModelValidationCmd(OasisBaseCommand):
+    """
+    Checks model data for validity.
+    """
+
+    def add_args(self, parser):
+        """
+        Adds arguments to the argument parser.
+
+        :param parser: The argument parser object
+        :type parser: ArgumentParser
+        """
+
+        super(self.__class__, self).add_args(parser)
+
+        parser.add_argument('-d', '--model-data-path', default=None, help='Directory containing additional user-supplied model data files')
+
+    def action(self, args):
+        """
+        Performs validity checks on model data csv files using ktools
+        executables.
+
+        :param args: The arguments from the command line
+        :type args: Namespace
+        """
+
+        inputs = InputValues(args)
+
+        model_data_fp = as_path(inputs.get('model_data_path', required=True, is_path=True), 'Model data path', is_dir=True)
+
+        csv_validity_test(model_data_fp)
+
+
 class TestCmd(BaseCommand):
     """
     Test models and keys servers
@@ -91,4 +131,5 @@ class TestCmd(BaseCommand):
 
     sub_commands = {
         'gen-model-tester-dockerfile': GenerateModelTesterDockerFileCmd,
+        'model-validation': ModelValidationCmd,
     }
