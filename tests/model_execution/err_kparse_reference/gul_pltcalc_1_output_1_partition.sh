114eac34b21695aec461636d5b0ffb7d6d2ed1d7
Sam Gamble <hexadessa@gmail.com>
2019-10-09 14:58:13 +0100

    WIP - update bash tests

---
+++ tests/model_execution/err_kparse_reference/gul_pltcalc_1_output_1_partition.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+find output/* ! -name '*summary-info*' -type f -exec rm -f {} +
+rm -R -f fifo/*
+rm -R -f work/*
+
+mkdir work/kat
+mkfifo fifo/gul_P1
+
+mkfifo fifo/gul_S1_summary_P1
+mkfifo fifo/gul_S1_summarypltcalc_P1
+mkfifo fifo/gul_S1_pltcalc_P1
+
+
+# --- Do ground up loss computes ---
+
+pltcalc < fifo/gul_S1_summarypltcalc_P1 > work/kat/gul_S1_pltcalc_P1 & pid1=$!
+
+tee < fifo/gul_S1_summary_P1 fifo/gul_S1_summarypltcalc_P1 > /dev/null & pid2=$!
+summarycalc -i  -1 fifo/gul_S1_summary_P1 < fifo/gul_P1 &
+
+eve 1 1 | getmodel | gulcalc -S100 -L100 -r -a1 -i - > fifo/gul_P1  &
+
+wait $pid1 $pid2
+
+
+# --- Do ground up loss kats ---
+
+kat work/kat/gul_S1_pltcalc_P1 > output/gul_S1_pltcalc.csv & kpid1=$!
+wait $kpid1
+
+
+
+set +e
+
+rm fifo/gul_P1
+
+rm fifo/gul_S1_summary_P1
+rm fifo/gul_S1_summarypltcalc_P1
+rm fifo/gul_S1_pltcalc_P1
+
+rm -rf work/kat
+
---
+++ tests/model_execution/tmp_kparse_reference/il_no_lec_1_output_1_partition.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+find output/* ! -name '*summary-info*' -type f -exec rm -f {} +
+rm -R -f fifo/*
+rm -R -f work/*
+
+mkdir work/kat
+mkfifo fifo/il_P1
+
+mkfifo fifo/il_S1_summary_P1
+mkfifo fifo/il_S1_summaryeltcalc_P1
+mkfifo fifo/il_S1_eltcalc_P1
+mkfifo fifo/il_S1_summarysummarycalc_P1
+mkfifo fifo/il_S1_summarycalc_P1
+mkfifo fifo/il_S1_summarypltcalc_P1
+mkfifo fifo/il_S1_pltcalc_P1
+
+mkdir work/il_S1_summaryaalcalc
+
+# --- Do insured loss computes ---
+
+eltcalc < fifo/il_S1_summaryeltcalc_P1 > work/kat/il_S1_eltcalc_P1 & pid1=$!
+summarycalctocsv < fifo/il_S1_summarysummarycalc_P1 > work/kat/il_S1_summarycalc_P1 & pid2=$!
+pltcalc < fifo/il_S1_summarypltcalc_P1 > work/kat/il_S1_pltcalc_P1 & pid3=$!
+
+tee < fifo/il_S1_summary_P1 fifo/il_S1_summaryeltcalc_P1 fifo/il_S1_summarypltcalc_P1 fifo/il_S1_summarysummarycalc_P1 work/il_S1_summaryaalcalc/P1.bin > /dev/null & pid4=$!
+summarycalc -f  -1 fifo/il_S1_summary_P1 < fifo/il_P1 &
+
+# --- Do ground up loss computes ---
+
+
+eve 1 1 | getmodel | gulcalc -S0 -L0 -r -a1 -i - | tee fifo/gul_P1 | fmcalc -a2 > fifo/il_P1  &
+
+wait $pid1 $pid2 $pid3 $pid4
+
+
+# --- Do insured loss kats ---
+
+kat work/kat/il_S1_eltcalc_P1 > output/il_S1_eltcalc.csv & kpid1=$!
+kat work/kat/il_S1_pltcalc_P1 > output/il_S1_pltcalc.csv & kpid2=$!
+kat work/kat/il_S1_summarycalc_P1 > output/il_S1_summarycalc.csv & kpid3=$!
+
+# --- Do ground up loss kats ---
+
+wait $kpid1 $kpid2 $kpid3
+
+
+aalcalc -Kil_S1_summaryaalcalc > output/il_S1_aalcalc.csv & lpid1=$!
+wait $lpid1
+
+
+set +e
+
+rm -rf work/kat
+
+rm fifo/il_P1
+
+rm fifo/il_S1_summary_P1
+rm fifo/il_S1_summaryeltcalc_P1
+rm fifo/il_S1_eltcalc_P1
+rm fifo/il_S1_summarysummarycalc_P1
+rm fifo/il_S1_summarycalc_P1
+rm fifo/il_S1_summarypltcalc_P1
+rm fifo/il_S1_pltcalc_P1
+
+rm -rf work/kat
+rm -rf work/il_S1_summaryaalcalc/*
+rmdir work/il_S1_summaryaalcalc
