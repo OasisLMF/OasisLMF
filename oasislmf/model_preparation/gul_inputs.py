aaadd3891853b4d8e0c8f8dfb0c7f0ffa653e0c9
Daniel Evans <daniel.evans@jbarisk.com>
2019-11-19 14:34:16 +0000

    Test column ordering fix

--- oasislmf/model_preparation/gul_inputs.py
+++ oasislmf/model_preparation/gul_inputs.py
@@ -209,7 +209,7 @@ def get_gul_input_items(
         # Select only the columns required. This reduces memory use significantly for portfolios
         # that include many OED columns.
         exposure_df_gul_inputs_cols = [
-            'loc_id', loc_num, acc_num, portfolio_num, cond_num
+            'loc_id', portfolio_num, acc_num, loc_num, cond_num
         ] + term_cols + tiv_cols
         if SOURCE_IDX['loc'] in exposure_df:
             exposure_df_gul_inputs_cols += [SOURCE_IDX['loc']]
