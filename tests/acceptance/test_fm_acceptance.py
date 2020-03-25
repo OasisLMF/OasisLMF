5759e4a80ac0f93ccfb6a68d5b7f75c256b11b01
joh <johanna.carter@oasislmf.org>
2020-03-25 18:25:28 +0000

    completed acceptance tests

--- tests/acceptance/test_fm_acceptance.py
+++ tests/acceptance/test_fm_acceptance.py
@@ -333,3 +333,28 @@ class FmAcceptanceTests(TestCase):
 
     def test_fm27(self):
         self.run_test('fm27')
+
+    def test_xx_32(self):
+        self.run_test('xx_32')
+
+    def test_xx_38(self):
+        self.run_test('xx_38')
+
+    def test_xx_39(self):
+        self.run_test('xx_39')
+
+    def test_xx_42(self):
+        self.run_test('xx_42')
+
+    def test_xx_43(self):
+        self.run_test('xx_43')
+
+    def test_xx_47(self):
+        self.run_test('xx_47')
+
+    def test_xx_53(self):
+        self.run_test('xx_53')
+
+    def test_xx_54(self):
+        self.run_test('xx_54')
+
