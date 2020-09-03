7dfd3e3b3d69b5b11edb45a79a7fe2329f537a1b
johcarter <johanna.carter@oasislmf.org>
2020-09-03 16:54:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.3.4'
+KTOOLS_VERSION = '3.3.4-fix-unlimited-loss'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
