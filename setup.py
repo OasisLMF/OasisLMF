32df93573f56c3ee6b3a201d4e2ce21f1f9d6f1a
johcarter <johanna.carter@oasislmf.org>
2020-09-15 13:52:07 +0100

    updated calc_rules_direct

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.3.5'
+KTOOLS_VERSION = '3.3.5-add-calcrules'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
