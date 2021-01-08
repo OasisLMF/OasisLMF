37ca71c70f9711e8200c3d5d5df8055cdc369741
Joh Carter <johanna.carter@oasislmf.org>
2021-01-08 12:56:17 +0000

    reorg2

---
+++ validation/utils/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to update the expected results of a list of test cases
+# Removes /expected and copies the contents of /run into /expected for those directories listed in list.txt
+filename='list.txt'
+n=1
+while read d; do
+# reading each line
+echo "Updating expected for $d"
+rm -r -f "$d/expected/"
+mkdir "$d/expected"
+cp -r "$d/run/"* "$d/expected/"
+n=$((n+1))
+done < $filename
+n=$((n-1))
+echo "$n test case expected results updated"
