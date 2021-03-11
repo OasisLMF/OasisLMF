aa4ad22ebda3ce4661a42bda7e8a41c79d0a250c
Sam Gamble <hexadessa@gmail.com>
2021-03-11 15:56:00 +0000

    Update test_fmpy and Remove non-matching intermediate files

--- tests/fm/test_fmpy.py
+++ tests/fm/test_fmpy.py
@@ -25,7 +25,7 @@ class FmAcceptanceTests(TestCase):
             shutil.copytree(tmp_run_dir, output_dir)
             print(f'Generated Output stored in: {output_dir}')
 
-    def run_test(self, test_case, fmpy=False):
+    def run_test(self, test_case, fmpy=False, subperils=1, expected_dir="expected"):
         with tempfile.TemporaryDirectory() as tmp_run_dir:
             result = OasisManager().run_fm_test(
                 test_case_dir=self.test_cases_fp,
@@ -34,10 +34,11 @@ class FmAcceptanceTests(TestCase):
                 update_expected=self.update_expected,
                 fmpy=fmpy,
                 fmpy_sort_output=True,
-                test_tolerance=0.0001 # one location has -0.0001% error in insurance_fm
+                num_subperils=subperils,
+                test_tolerance=0.0001,
+                expected_output_dir=expected_dir,
             )
             self._store_output(test_case, tmp_run_dir)
-
         self.assertTrue(result)
 
     def test_insurance(self):
@@ -52,5 +53,16 @@ class FmAcceptanceTests(TestCase):
     def test_reinsurance2(self):
         self.run_test('reinsurance2',fmpy=True)
 
+    # multiperil tests 
+    def test_insurance_2_subperils(self):
+        self.run_test('insurance', fmpy=True, subperils=2, expected_dir="expected_subperils")
 
+    def test_insurance_step_2_subperils(self):
+        self.run_test('insurance_step', fmpy=True, subperils=2, expected_dir="expected_subperils")
+
+    def test_reinsurance1_2_subperils(self):
+        self.run_test('reinsurance1', fmpy=True, subperils=2, expected_dir="expected_subperils")
+
+    def test_reinsurance2_2_subperils(self):
+        self.run_test('reinsurance2', fmpy=True, subperils=2, expected_dir="expected_subperils")
 
