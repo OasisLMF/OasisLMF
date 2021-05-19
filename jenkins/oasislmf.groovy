d3e240eda346b2da5179fd8ddec2364a5a29310b
sambles <sambles@users.noreply.github.com>
2021-05-19 11:42:11 +0100

    Automate Change logs and release notes (#826)
    * Add issue and pull request templates
    
    * update
    
    * Update
    
    * Add comment for "last working configuration"
    
    * Fix pr template
    
    * Auto changelog / release notes
    
    * Add notes on release process
    
    * fix typo
    
    * typo

--- jenkins/oasislmf.groovy
+++ jenkins/oasislmf.groovy
@@ -11,22 +11,21 @@ node {
 
     properties([
       parameters([
-        [$class: 'StringParameterDefinition',  name: 'BUILD_BRANCH', defaultValue: 'master'],
-        [$class: 'StringParameterDefinition',  name: 'SOURCE_BRANCH', defaultValue: BRANCH_NAME],
-        [$class: 'StringParameterDefinition',  name: 'PIWIND_BRANCH', defaultValue: set_piwind_branch],
-        [$class: 'StringParameterDefinition',  name: 'PUBLISH_VERSION', defaultValue: ''],
-        [$class: 'StringParameterDefinition',  name: 'KTOOLS_VERSION', defaultValue: ''],
-        [$class: 'StringParameterDefinition',  name: 'GPG_KEY', defaultValue: 'gpg-privatekey'],
-        [$class: 'StringParameterDefinition',  name: 'GPG_PASSPHRASE', defaultValue: 'gpg-passphrase'],
-        [$class: 'StringParameterDefinition',  name: 'TWINE_ACCOUNT', defaultValue: 'sams_twine_account'],
-        [$class: 'BooleanParameterDefinition', name: 'PURGE', defaultValue: Boolean.valueOf(false)],
-        [$class: 'BooleanParameterDefinition', name: 'PUBLISH', defaultValue: Boolean.valueOf(false)],
-        [$class: 'BooleanParameterDefinition', name: 'AUTO_MERGE', defaultValue: Boolean.valueOf(true)],
-        [$class: 'BooleanParameterDefinition', name: 'SLACK_MESSAGE', defaultValue: Boolean.valueOf(false)]
+        [$class: 'StringParameterDefinition',  description: "Oasis Build scripts branch",        name: 'BUILD_BRANCH', defaultValue: 'master'],
+        [$class: 'StringParameterDefinition',  description: "OasisLMF repo branch",              name: 'SOURCE_BRANCH', defaultValue: BRANCH_NAME],
+        [$class: 'StringParameterDefinition',  description: "Test against piwind branch",        name: 'PIWIND_BRANCH', defaultValue: set_piwind_branch],
+        [$class: 'StringParameterDefinition',  description: "Release Version",                   name: 'PUBLISH_VERSION', defaultValue: ''],
+        [$class: 'StringParameterDefinition',  description: "Last released version",             name: 'PREV_VERSION', defaultValue: ''],
+        [$class: 'StringParameterDefinition',  description: "Ktools version to install",         name: 'KTOOLS_VERSION', defaultValue: ''],
+        [$class: 'StringParameterDefinition',  description: "Jenkins credential for GPG",        name: 'GPG_KEY', defaultValue: 'gpg-privatekey'],
+        [$class: 'StringParameterDefinition',  description: "Jenkins credential for passphrase", name: 'GPG_PASSPHRASE', defaultValue: 'gpg-passphrase'],
+        [$class: 'StringParameterDefinition',  description: "Jenkins credentials Twine",         name: 'TWINE_ACCOUNT', defaultValue: 'sams_twine_account'],
+        [$class: 'BooleanParameterDefinition', description: "Create release if checked",         name: 'PUBLISH', defaultValue: Boolean.valueOf(false)],
+        [$class: 'BooleanParameterDefinition', description: "Mark as pre-released software",     name: 'PRE_RELEASE', defaultValue: Boolean.valueOf(true)],
+        [$class: 'BooleanParameterDefinition', description: "Perform a gitflow merge",           name: 'AUTO_MERGE', defaultValue: Boolean.valueOf(true)],
+        [$class: 'BooleanParameterDefinition', description: "Send build status to slack",        name: 'SLACK_MESSAGE', defaultValue: Boolean.valueOf(false)]
       ])
     ])
-
-
 
     // Build vars
     String build_repo = 'git@github.com:OasisLMF/build.git'
@@ -54,15 +53,23 @@ node {
     String MDK_BRANCH = source_branch
     if (source_branch.matches("PR-[0-9]+")){
         MDK_BRANCH = "refs/pull/$CHANGE_ID/merge"
-    }    
+    }
 
     //env.PYTHON_ENV_DIR = "${script_dir}/pyth-env"           // Virtualenv location
     env.PIPELINE_LOAD =  script_dir + source_sh             // required for pipeline.sh calls
     sh 'env'
 
-    if (params.PUBLISH && ! ( source_branch.matches("release/(.*)") || source_branch.matches("hotfix/(.*)") || source_branch.matches("backports/(.*)") ) ){
-        // fail fast, only branches named `release/*` are valid for publish
-        sh "echo `Publish Only allowed on a release/* branch`"
+    if (! params.PRE_RELEASE) {
+        if (params.PUBLISH && ! ( source_branch.matches("release/(.*)") || source_branch.matches("hotfix/(.*)") || source_branch.matches("backports/(.*)")) ){
+            // fail fast, only branches named `release/*` are valid for publish
+            sh "echo `Publish Only allowed on a release/* branch`"
+            sh "exit 1"
+        }
+    }
+
+    //make sure release candidate versions are tagged correctly
+    if (params.PUBLISH && params.PRE_RELEASE && ! vers_pypi.matches('^(\\d+\\.)(\\d+\\.)(\\*|\\d+)rc(\\d+)$')) {
+        sh "echo release candidates must be tagged {version}rc{N}, example: 1.0.0rc1"
         sh "exit 1"
     }
 
@@ -88,8 +95,8 @@ node {
                             } else {
                                 // Checkout branch
                                 sh "git checkout ${source_branch}"
-                            }   
-                        }   
+                            }
+                        }
                     }
                 }
             }
@@ -103,13 +110,13 @@ node {
                 } else {
                     println("Keep current version numbers")
                 }
-                
+
                 // Load versions as set in files
                 if (! vers_pypi?.trim() && params.PUBLISH){
                     vers_file = readFile("oasislmf/__init__.py")
                     vers_pypi = vers_file.trim().split("'")[-1]
                     println("Loaded package version from file: $vers_pypi")
-                }    
+                }
             }
         }
 
@@ -132,7 +139,16 @@ node {
         //
         // gpg_key  --> Jenkins credentialId  type 'Secret file', GPG key
         // gpg_pass --> Jenkins credentialId  type 'Secret text', passphrase for the above key
+
         if (params.PUBLISH){
+            // Build chanagelog image
+            stage("Create Changelog builder") {
+                dir(build_workspace) {
+                    sh "docker build -f docker/Dockerfile.release-notes -t release-builder ."
+                }
+            }
+
+            // GPG sign pip package
             stage('Sign Package: ' + source_func) {
                 String gpg_dir='/var/lib/jenkins/.gnupg/'
                 sh "if test -d ${gpg_dir}; then rm -r ${gpg_dir}; fi"
@@ -160,9 +176,32 @@ node {
                     }
                 }
             }
+            // Tag release
+            stage('Tag release'){
+                dir(source_workspace) {
+                    sshagent (credentials: [git_creds]) {
+                        sh "git tag ${vers_pypi}"
+                        sh "git push origin ${vers_pypi}"
+                    }
+                }
+            }
+            // Create release notes
+            stage('Create Changelog'){
+                dir(source_workspace) {
+                    withCredentials([string(credentialsId: 'github-api-token', variable: 'gh_token')]) {
+                        sh "docker run -v ${env.WORKSPACE}/${source_workspace}:/tmp release-builder build-changelog --repo OasisLMF --from-tag ${params.PREV_VERSION} --to-tag ${vers_pypi} --github-token ${gh_token} --local-repo-path ./ --output-path ./CHANGELOG.rst --apply-milestone"
+                        sh "docker run -v ${env.WORKSPACE}/${source_workspace}:/tmp release-builder build-release --repo OasisLMF --from-tag ${params.PREV_VERSION} --to-tag ${vers_pypi} --github-token ${gh_token} --local-repo-path ./ --output-path ./RELEASE.md"
+                    }
+                    sshagent (credentials: [git_creds]) {
+                        sh "git add ./CHANGELOG.rst"
+                        sh "git commit -m 'Update changelog ${vers_pypi}'"
+                        sh "git push"
+                    }
+                }
+            }
 
-            // Create GitHub release 
-            stage("Create Release: GitHub") { 
+            // Create GitHub release
+            stage("Create Release: GitHub") {
                 // Tag repo
                 sshagent (credentials: [git_creds]) {
                     dir(source_workspace) {
@@ -174,21 +213,18 @@ node {
                 withCredentials([string(credentialsId: 'github-api-token', variable: 'gh_token')]) {
                     String repo = "OasisLMF/OasisLMF"
 
-                    // Create release 
+                    // Create release
                     def json_request = readJSON text: '{}'
+                    def release_body = readFile(file: "${env.WORKSPACE}/${source_workspace}/RELEASE.md")
+
                     json_request['tag_name'] = vers_pypi
                     json_request['target_commitish'] = 'master'
                     json_request['name'] = vers_pypi
-                    json_request['body'] = ""
+                    json_request['body'] = release_body
                     json_request['draft'] = false
                     json_request['prerelease'] = false
                     writeJSON file: 'gh_request.json', json: json_request
                     sh 'curl -XPOST -H "Authorization:token ' + gh_token + "\" --data @gh_request.json https://api.github.com/repos/$repo/releases > gh_response.json"
-
-                   // Create milestone
-                    dir(source_workspace) {
-                        sh PIPELINE + " create_milestone ${gh_token} ${repo} ${vers_pypi} CHANGELOG.rst"
-                    }
                 }
             }
         }
@@ -212,16 +248,24 @@ node {
         }
 
         // Run merge back if publish
-         if (params.PUBLISH && params.AUTO_MERGE && ! hasFailed){ 
+         if (params.PUBLISH && params.AUTO_MERGE && ! hasFailed){
             dir(source_workspace) {
                 sshagent (credentials: [git_creds]) {
-                    sh "git stash"
-                    sh "git checkout master && git pull"
-                    sh "git merge ${source_branch} && git push"
-                    sh "git checkout develop && git pull"
-                    sh "git merge master && git push"
-                }   
-            }   
-        }   
+                    if (! params.PRE_RELEASE) {
+                        // Release merge back into master
+                        sh "git stash"
+                        sh "git checkout master && git pull"
+                        sh "git merge ${source_branch} && git push"
+                        sh "git checkout develop && git pull"
+                        sh "git merge master && git push"
+                    } else {
+                        // pre_pelease merge back into develop
+                        sh "git stash"
+                        sh "git checkout develop && git pull"
+                        sh "git merge ${source_branch} && git push"
+                    }
+                }
+            }
+        }
     }
 }
