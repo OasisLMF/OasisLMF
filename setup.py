e47a9e91e47b9fc0d7d8af09af31a1cb9e7e6f42
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2021-05-27 11:37:27 +0100

    Set ktools version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.6.0'
+KTOOLS_VERSION = '3.6.1rc1'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
