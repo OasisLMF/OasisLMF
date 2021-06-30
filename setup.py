498bf2506246b12a182ef2f31cf24717bea19525
Sam Gamble <hexadessa@gmail.com>
2021-06-30 11:32:45 +0100

    set ktools to testing tag

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.6.1'
+KTOOLS_VERSION = '3.6.2rc1'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
