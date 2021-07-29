4c58eda7ed1b196a1ad9a6ff5bc3aa635dff92f4
Sam Gamble <hexadessa@gmail.com>
2021-07-29 11:41:29 +0100

    Add missing func to listed

--- oasislmf/cli/model.py
+++ oasislmf/cli/model.py
@@ -3,6 +3,7 @@ __all__ = [
     'GenerateKeysCmd',
     'GenerateLossesCmd',
     'GenerateLossesPartialCmd',
+    'GenerateLossesOutputCmd',
     'GenerateOasisFilesCmd',
     'ModelCmd',
     'RunCmd'
