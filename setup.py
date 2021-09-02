30d8af7fad30ff43b5cf3a3e746812c0aafa446c
Sam Gamble <hexadessa@gmail.com>
2021-09-02 17:05:13 +0100

    Set version 1.20.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.6.4rc1'
+KTOOLS_VERSION = '3.6.4'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
