9e265bbc7dc0d4e7687007009ce3bbf26e49654b
maxwellflitton <maxwellflitton@gmail.com>
2021-11-17 15:04:25 +0000

    adding modelpy flag

--- oasislmf/pytools/modelpy.py
+++ oasislmf/pytools/modelpy.py
@@ -13,7 +13,12 @@ parser.add_argument('-v', '--logging-level', help='logging level (debug:10, info
                     default=30, type=int)
 
 
-def main():
+def main() -> None:
+    """
+    Is the entry point for the modelpy command which loads data and constructs a model.
+
+    Returns: None
+    """
     kwargs = vars(parser.parse_args())
 
     # add handler to fm logger
