6a51d41d03417ecad9b2db2199af581db423b5b8
maxwellflitton <maxwellflitton@gmail.com>
2021-12-16 12:49:31 +0000

    increasing the connections

--- oasislmf/pytools/data_layer/footprint_layer.py
+++ oasislmf/pytools/data_layer/footprint_layer.py
@@ -25,6 +25,7 @@ logging.basicConfig(
 POINTER_PATH = str(os.path.dirname(os.path.realpath(__file__))) + "/pointer_flag.txt"
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8080
+PROCESSES_SUPPORTED = 100
 
 
 class OperationEnum(Enum):
@@ -85,7 +86,7 @@ class FootprintLayer:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (TCP_IP, TCP_PORT)
         self.socket.bind(server_address)
-        self.socket.listen(1)
+        self.socket.listen(PROCESSES_SUPPORTED)
 
     def _establish_shutdown_procedure(self) -> None:
         """
