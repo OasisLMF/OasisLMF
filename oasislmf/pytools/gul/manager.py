07153d91e8dd6b5c659b8efbd389b49411f8fae0
mtazzari <mtazzari@gmail.com>
2022-02-02 14:47:26 +0000

    [gulpy] add get_coverages function

--- oasislmf/pytools/gul/manager.py
+++ oasislmf/pytools/gul/manager.py
@@ -3,10 +3,42 @@ This file is the entry point for the gul command for the package.
 
 """
 
+from oasislmf.pytools.getmodel.common import oasis_float
+from oasislmf.pytools.getmodel.manager import get_mean_damage_bins, get_items
+
+import os
+import numpy as np
 import logging
 logger = logging.getLogger(__name__)
 
-print("[gulpy] Hello world!")
+
+def get_coverages(input_path, ignore_file_type=set()):
+    """
+    Loads the coverages from the coverages file.
+
+    Args:
+        input_path: (str) the path containing the coverage file
+        ignore_file_type: set(str) file extension to ignore when loading
+
+    Returns: (np.array[oasis_float])
+        coverages array
+    """
+    input_files = set(os.listdir(input_path))
+
+    # TODO: store default filenames (e.g., coverages.bin) in a parameters file
+
+    if "coverages.bin" in input_files and "bin" not in ignore_file_type:
+        logger.debug(f"loading {os.path.join(input_path, 'coverages.csv')}")
+        coverages = np.fromfile(os.path.join(input_path, "coverages.bin"), dtype=oasis_float)
+
+    elif "coverages.csv" in input_files and "csv" not in ignore_file_type:
+        logger.debug(f"loading {os.path.join(input_path, 'coverages.csv')}")
+        coverages = np.genfromtxt(os.path.join(input_path, "coverages.csv"), dtype=oasis_float, delimiter=",")
+
+    else:
+        raise FileNotFoundError(f'coverages file not found at {input_path}')
+
+    return coverages
 
 
 def run(*args, **kwargs):
