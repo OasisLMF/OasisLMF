f184a674e64c2510c136eab0adc0352a19bec15e
Marco Tazzari <mtazzari@users.noreply.github.com>
2022-02-09 09:08:38 +0000

    Add cdftocsvpy tool (#985)
    * [pytools] Add cdftocsvpy
    
    * [pytools] Use frombuffer to get stream_type
    
    * [pytools] Use readinto instead of readinto1
    
    * [pytools] Remove logging and unwanted options
    
    * [pytools] Improve help message for run_dir arg

---
+++ oasislmf/pytools/gul/common.py
@@ -0,0 +1,18 @@
+"""
+This file defines the data types that are loaded from the data files.
+"""
+import os
+
+import numba as nb
+import numpy as np
+
+from oasislmf.pytools.getmodel.common import oasis_float, areaperil_int
+
+ProbMean = nb.from_dtype(np.dtype([('prob_to', oasis_float),
+                                   ('bin_mean', oasis_float)
+                                   ]))
+
+damagecdfrec = nb.from_dtype(np.dtype([('event_id', np.int32),
+                                       ('areaperil_id', areaperil_int),
+                                       ('vulnerability_id', np.int32)
+                                       ]))
