f16cea554d8810fbfef58b88a7017dbfc65f1c50
mtazzari <mtazzari@gmail.com>
2022-03-21 17:58:22 +0000

    [gulpy] Read getmodel stream more efficiently

--- oasislmf/pytools/gul/manager.py
+++ oasislmf/pytools/gul/manager.py
@@ -20,9 +20,9 @@ from scipy.stats import qmc
 from math import sqrt, ceil, log2  # faster than numpy.sqrt
 
 from oasislmf.pytools.getmodel.manager import get_mean_damage_bins, get_damage_bins, get_items, Item
-from oasislmf.pytools.getmodel.common import oasis_float
+from oasislmf.pytools.getmodel.common import oasis_float, areaperil_int
 from oasislmf.pytools.gul.common import gulSampleslevelHeader, gulSampleslevelRec, gulSampleFullRecord
-from oasislmf.pytools.gul.common import processrecData, gulItemIDLoss
+from oasislmf.pytools.gul.common import processrecData, gulItemIDLoss, oasis_float_to_int_size
 
 from oasislmf.pytools.gul.common import ProbMean, damagecdfrec, Item_map_rec
 
@@ -46,59 +46,170 @@ EVENT_ID_HASH_CODE = np.int64(1943272559)
 HASH_MOD_CODE = np.int64(2147483648)
 
 
-def read_getmodel_stream(run_dir, streams_in):
-    """Read the getmoudel output stream.
+def read_getmodel_stream(run_dir, streams_in, buff_size=65536):
+    """Read the getmodel output stream.
 
     Args:
         run_dir ([str]): Path to the run directory.
+        streams_in ([]): input streams
 
     Raises:
         ValueError: If the stream type is not 1.
 
-    """
-    # TODO: read in chunks of buffsize and keep yielding record by record
+    Yields:
+        All the cdfs related to 1 event.
 
-    # get damage bins from file
+        damagecdfs: array-like([damagecdf])
+        Nbinss: array-like([int]) 
+        recs: array-like([ProbMean])
+
+    """
+    # determine stream type
+    stream_type = np.frombuffer(streams_in.read(4), dtype='i4')
+
+    # TODO: make sure the bit1 and bit 2-4 compliance is checked
+    # see https://github.com/OasisLMF/ktools/blob/master/docs/md/CoreComponents.md
+    if stream_type[0] != 1:
+        raise ValueError(f"FATAL: Invalid stream type: expect 1, got {stream_type[0]}.")
+
+    # get damage bins from fileFIXME
     static_path = os.path.join(run_dir, 'static')
     damage_bins = get_damage_bins(static_path=static_path)
 
     # maximum number of damage bins (individual items can have up to `total_bins` bins)
     if damage_bins.shape[0] == 0:
-        total_bins = 1000
+        max_Nbins = 1000
     else:
-        total_bins = damage_bins.shape[0]
+        max_Nbins = damage_bins.shape[0]
 
-    # determine stream type
-    stream_type = np.frombuffer(streams_in.read(4), dtype='i4')
+    # maximum number of entries is buff_size divided by the minimum entry size
+    # (corresponding to a 1-bin only cdf)
+    min_size_cdf_entry = damagecdfrec.size + ProbMean.size + 4
 
-    # FIXME: make sure the bit1 and bit 2-4 compliance is checked
-    # see https://github.com/OasisLMF/ktools/blob/master/docs/md/CoreComponents.md
-    if stream_type[0] != 1:
-        raise ValueError(f"FATAL: Invalid stream type: expect 1, got {stream_type[0]}.")
-
-    # prepare all the data buffers
-    damagecdf_mv = memoryview(bytearray(damagecdfrec.size))
-    damagecdf = np.ndarray(1, buffer=damagecdf_mv, dtype=damagecdfrec.dtype)
-    Nbins_mv = memoryview(bytearray(4))
-    Nbins = np.ndarray(1, buffer=Nbins_mv, dtype='i4')
-    rec_mv = memoryview(bytearray(total_bins * ProbMean.size))
-    rec = np.ndarray(total_bins, buffer=rec_mv, dtype=ProbMean.dtype)
-
-    # start reading the stream
-    # each record from getmodel is expected to contain:
+    # each record from getmodel stream is expected to contain:
     # 1 damagecdfrec obj, 1 int (Nbins), a number `Nbins` of ProbMean objects
-    while True:
-        len_read = streams_in.readinto(damagecdf_mv)
-        len_read = streams_in.readinto(Nbins_mv)
-        len_read = streams_in.readinto(rec_mv[:Nbins[0] * ProbMean.size])
 
-        # exit if the stream has ended
-        if len_read == 0:
+    # use a memoryview of size twice the buff_size to ensure `read_into1` always reads the maximum amount
+    # data possible (the largest between the pipe limit and the remaining memory to fill the memoryview)
+    mv = memoryview(bytearray(buff_size * 2))
+    int32_mv = np.ndarray(buff_size * 2 // 4, buffer=mv, dtype='i4')
+
+    cursor = 0
+    valid_buf = 0
+    last_event_id = -1
+    damagecdfs, Nbinss, recs = [], [], []
+    len_read = 1
+    size_cdf_entry = min_size_cdf_entry
+    while True:
+        if len_read > 0:
+            # read stream from valid_buf onwards
+            len_read = streams_in.readinto1(mv[valid_buf:])
+            # extend the valid_buf by the same amount of data that was read
+            valid_buf += len_read
+
+        # read the streamed data into formatted data
+        cursor, i, yield_event, event_id, damagecdf, Nbins, rec, last_event_id = stream_to_data(
+            int32_mv, valid_buf, size_cdf_entry, max_Nbins, last_event_id)
+
+        if yield_event:
+            # event is fully read, append the last chunk of data to the list of this event
+            damagecdfs.append(damagecdf[:i])
+            Nbinss.append(Nbins[:i])
+            recs.append(rec[:i])
+
+            yield np.concatenate(damagecdfs), np.concatenate(Nbinss), np.concatenate(recs)
+
+            # start a new list for the new event, storing the first element
+            damagecdfs, Nbinss, recs = [damagecdf[i:i + 1]], [Nbins[i:i + 1]], [rec[i:i + 1]]
+            last_event_id = event_id
+
+        else:
+            # the current event is not finished, keep appending data about this event
+            damagecdfs.append(damagecdf[:i + 1])
+            Nbinss.append(Nbins[:i + 1])
+            recs.append(rec[:i + 1])
+
+        # convert cursor to bytes
+        cursor_buf = cursor * int32_mv.itemsize
+
+        if valid_buf == cursor_buf:
+            # this is the last cycle, all data has been read, append the last chunk of data
+            damagecdfs.append(damagecdf[:i])
+            Nbinss.append(Nbins[:i])
+            recs.append(rec[:i])
+
+            yield np.concatenate(damagecdfs), np.concatenate(Nbinss), np.concatenate(recs)
             break
 
-        yield damagecdf, Nbins, rec
+        else:
+            # this is not the last cycle
+            # move the un-read data to the beginning of the memoryview
+            mv[:valid_buf - cursor_buf] = mv[cursor_buf:valid_buf]
 
-    return 0
+            # update the length of the valid data
+            valid_buf -= cursor_buf
+
+
+@nb.njit(cache=True, fastmath=True)
+def stream_to_data(int32_mv, valid_buf, size_cdf_entry, max_Nbins, last_event_id):
+    """Read streamed data into formatted data.
+
+    Args:
+        int32_mv (ndarray): int32 view of the buffer
+        valid_buf (int): number of bytes with valid data
+        size_cdf_entry (int): size (in bytes) of a single record
+        max_Nbins (int): Maximum number of probability bins
+        last_event_id (int): event_id of the last event that was completed
+
+    Returns:
+        cursor (int): number of int numbers read from the int32_mv ndarray.
+        i (int): number of cdf data entries read.
+        yield_event (bool): if True, the current event (id=`event_id`) is completed.
+        event_id (int): id of the event being read.
+        damagecdf (array-like[damagecdf]):
+        Nbins (array-like[damagecdf]):
+        rec (array-like[damagecdf]):
+        last_event_id (int): event_id of the last event that was completed
+
+    # TODO: don't store event_id (it's wasted space). need to change this everywhere `damagecdfrec` is used
+
+    """
+    valid_len = valid_buf // size_cdf_entry
+    yield_event = False
+
+    Nbins = np.zeros(valid_len, dtype='i4')
+    damagecdf = np.zeros(valid_len, dtype=damagecdfrec)
+    rec = np.zeros((valid_len, max_Nbins), dtype=ProbMean)
+
+    i = 0
+    cursor = 0
+    while cursor < valid_len:
+        damagecdf[i]['event_id'], cursor = int32_mv[cursor], cursor + 1
+        damagecdf[i]['areaperil_id'], cursor = int32_mv[cursor:cursor + 1].view(areaperil_int)[0], cursor + 1
+        damagecdf[i]['vulnerability_id'], cursor = int32_mv[cursor], cursor + 1
+        event_id = damagecdf[i]['event_id']
+        Nbins[i] = int32_mv[cursor]
+        cursor += 1
+
+        for j in range(Nbins[i]):
+            rec[i, j]['prob_to'] = int32_mv[cursor: cursor + oasis_float_to_int_size].view(oasis_float)[0]
+            cursor += oasis_float_to_int_size
+            rec[i, j]['bin_mean'] = int32_mv[cursor: cursor + oasis_float_to_int_size].view(oasis_float)[0]
+            cursor += oasis_float_to_int_size
+
+        if event_id != last_event_id:
+            # a new event has started
+            if last_event_id > 0:
+                # this is not the beginning of first event
+                yield_event = True
+
+                return cursor, i, yield_event, event_id, damagecdf, Nbins, rec, last_event_id
+
+            last_event_id = event_id
+
+        i += 1
+
+    return cursor, i - 1, yield_event, event_id, damagecdf, Nbins, rec, last_event_id
 
 
 def get_coverages(input_path, ignore_file_type=set()):
