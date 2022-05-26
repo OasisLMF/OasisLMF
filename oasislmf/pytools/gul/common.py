22252ef37e327cbd9c1f4a0bb0170c47daeeb498
Marco Tazzari <6020226+mtazzari@users.noreply.github.com>
2022-05-26 14:29:50 +0100

    Improve gulpy memory usage and performance (#1033)
    * [gulpy] refactor to reduce memory footprint
    
    * allocate memory once via np array
    
    * [gulpy] bugfix
    
    * [gulpy] bugfix 2
    
    * [gulpy] trying to solve stream issue
    
    * use valid buff as look breaker (#1037)
    
    * use valid buff as look breaker
    
    * add error if stream stop prematurely
    
    Co-authored-by: sstruzik <stephane.struzik@gmail.com>
    
    * [gulpy] update common.py
    
    * [gulpy] update manager.py
    
    * [gulpy] update io.py
    
    * [gulpy] fix logging
    
    * [utils] bugfix
    
    * [gulpy] add binary search
    
    * [gulpy] use binary search
    
    * [gulpy] binary search bugfix
    
    * [gulpy] bugfix binary search
    
    * [gulpy] bugfix
    
    * [gulpy] fix binary search
    
    * [gulpy] us areaperil_int to advance cursor
    
    * [gulpy] cahce a division in split_tiv
    
    * [gulpy] bugfix
    
    * [gulpy] updating docs - WIP
    
    * [gulpy] cleanup, remove LossWriter
    
    * [gulpy] do not need damagebin dict and maxNbins
    
    * [gulpy] stream_to_data does not need len_read
    
    * [gulpy] read_getmodel_stream does not need run_dir
    
    * [gulpy] cleanup; update all docstrings
    
    * [gulpy] remove insert-sort items-data
    
    Co-authored-by: sstruzik <stephane.struzik@gmail.com>

--- oasislmf/pytools/gul/common.py
+++ oasislmf/pytools/gul/common.py
@@ -7,9 +7,26 @@ import numba as nb
 
 from oasislmf.pytools.getmodel.common import oasis_float, areaperil_int
 
-ITEM_ID_TYPE = nb.types.int32
-ITEMS_DATA_MAP_TYPE = nb.types.UniTuple(nb.types.int64, 2)
-COVERAGE_ID_TYPE = nb.types.int32
+# gul stream type
+# probably need to set this dynamically depending on the stream type
+gul_header = np.int32(1 | 2 << 24).tobytes()
+
+PIPE_CAPACITY = 65536  # bytes
+GETMODEL_STREAM_BUFF_SIZE = 2 * PIPE_CAPACITY
+GULPY_STREAM_BUFF_SIZE_WRITE = PIPE_CAPACITY
+
+items_data_type = nb.from_dtype(np.dtype([('item_id', np.int32),
+                                          ('damagecdf_i', np.int32),
+                                          ('rng_index', np.int32)
+                                          ]))
+
+coverage_type = nb.from_dtype(np.dtype([('tiv', np.float),
+                                        ('max_items', np.int32),
+                                        ('start_items', np.int32),
+                                        ('cur_items', np.int32)
+                                        ]))
+
+NP_BASE_ARRAY_SIZE = 8
 
 # negative sidx (definition)
 MEAN_IDX = -1
@@ -20,23 +37,17 @@ MAX_LOSS_IDX = -5
 
 NUM_IDX = 5
 
-# negative sidx + NUM_IDX
-SHIFTED_MEAN_IDX = MEAN_IDX + NUM_IDX
-SHIFTED_STD_DEV_IDX = STD_DEV_IDX + NUM_IDX
-SHIFTED_TIV_IDX = TIV_IDX + NUM_IDX
-SHIFTED_CHANCE_OF_LOSS_IDX = CHANCE_OF_LOSS_IDX + NUM_IDX
-SHIFTED_MAX_LOSS_IDX = MAX_LOSS_IDX + NUM_IDX
-
-
-ITEM_MAP_KEY_TYPE = nb.types.Tuple((nb.types.uint32, nb.types.int32))
+ITEM_MAP_KEY_TYPE = nb.types.Tuple((nb.from_dtype(areaperil_int), nb.types.int32))
 ITEM_MAP_VALUE_TYPE = nb.types.UniTuple(nb.types.int32, 3)
 
-# compute the relative size of oasis_float vs int32
+# compute the relative size of oasis_float and areaperil_int vs int32
 oasis_float_to_int32_size = oasis_float.itemsize // np.int32().itemsize
+areaperil_int_to_int32_size = areaperil_int.itemsize // np.int32().itemsize
 
 ProbMean = nb.from_dtype(np.dtype([('prob_to', oasis_float),
                                    ('bin_mean', oasis_float)
                                    ]))
+ProbMean_size = ProbMean.size
 
 damagecdfrec_stream = nb.from_dtype(np.dtype([('event_id', np.int32),
                                               ('areaperil_id', areaperil_int),
