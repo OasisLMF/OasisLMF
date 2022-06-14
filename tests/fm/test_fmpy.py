354c5ca30931040a74c1a20bd59f3bee232f2548
Joh Carter <johanna.carter@oasislmf.org>
2022-06-14 16:44:44 +0100

    updated input files for existing tests and add policy coverage tests to pytest excl cond1-5

--- tests/fm/test_fmpy.py
+++ tests/fm/test_fmpy.py
@@ -63,6 +63,9 @@ class FmAcceptanceTests(TestCase):
     def test_issues(self):
         self.run_test('issues',fmpy=True)
 
+    def test_insurance_policy_coverage(self):
+        self.run_test('insurance_policy_coverage',fmpy=True)
+
     # multiperil tests
     # superceded by insurance_and_step
     # def test_insurance_2_subperils(self):
@@ -75,7 +78,6 @@ class FmAcceptanceTests(TestCase):
     # def test_insurance_step_2_subperils(self):
     #     self.run_test('insurance_step', fmpy=True, subperils=2, expected_dir="expected_subperils")
 
-   # Bug under investigation
     def test_insurance_and_step_2_subperils(self):
         self.run_test('insurance_and_step', fmpy=True, subperils=2, expected_dir="expected_subperils")
 
@@ -87,3 +89,6 @@ class FmAcceptanceTests(TestCase):
 
     def test_issues_2_subperils(self):
         self.run_test('issues', fmpy=True, subperils=2, expected_dir="expected_subperils")
+
+    def test_insurance_policy_coverage_2_subperils(self):
+        self.run_test('insurance_policy_coverage',fmpy=True, subperils=2, expected_dir="expected_subperils")
