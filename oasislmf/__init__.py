3a28366bedb95ae5d562f1357f243b9770ea8345
Sam Gamble <hexadessa@gmail.com>
2022-07-07 10:54:13 +0100

    Revert "Fix ImportWarning"
    This reverts commit 4f18903da33a68cd086b78b8ade2c0ca3052c090.

--- oasislmf/__init__.py
+++ oasislmf/__init__.py
@@ -3,7 +3,6 @@ __version__ = '1.26.0'
 import sys
 import os
 from importlib.abc import MetaPathFinder, Loader
-from importlib.util import spec_from_loader
 import importlib
 import warnings
 
@@ -13,8 +12,6 @@ from logging import NullHandler
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
-
-# https://docs.python.org/3/library/importlib.html#importlib.machinery.PathFinder
 
 class MyLoader(Loader):
 
@@ -33,14 +30,6 @@ class MyLoader(Loader):
         sys.modules[old_name] = module
         return module
 
-    def exec_module(self, module):
-        old_name = fullname = module.__name__
-        names = fullname.split(".")
-        names[1] = self.sub_module
-        fullname = ".".join(names)
-        module = importlib.import_module(fullname)
-        sys.modules[old_name] = module
-        return module
 
 class MyImport(MetaPathFinder):
     """ Support alias of depreciated sub-modules
@@ -62,18 +51,6 @@ class MyImport(MetaPathFinder):
             "api": "platform",
             "platform": "platform_api"
         }
-
-    def find_spec(self, fullname, path=None, target=None):
-        names = fullname.split(".")
-        if len(names) >= 2:
-            if names[0] == "oasislmf" and names[1] in self.depricated_modules:
-                deprecated = "imports from 'oasislmf.{}' are deprecated. Import by using 'oasislmf.{}' instead.".format(
-                    names[1],
-                    self.depricated_modules[names[1]]
-                )
-                warnings.simplefilter("always")
-                warnings.warn(deprecated)
-                return spec_from_loader(fullname, MyLoader(self.depricated_modules[names[1]]))
 
     def find_module(self, fullname, path=None):
         names = fullname.split(".")
