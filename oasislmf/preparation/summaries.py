21bc2929fc3413a0b993fbc1e947b02fbbb74ab8
Sam Gamble <hexadessa@gmail.com>
2022-08-02 15:43:46 +0100

    Add IL columns to "GUL" summary section & fix non-oed column detection

--- oasislmf/preparation/summaries.py
+++ oasislmf/preparation/summaries.py
@@ -257,31 +257,24 @@ def write_summary_levels(exposure_df, accounts_fp, target_dir):
     }
 
     # GUL perspective (loc columns only)
-    #l_col_list = exposure_df.loc[:, exposure_df.any()].columns.to_list()
-    # NOTE: work around for pandas==1.2.0, any() not returning return the 'category' field types
     l_col_list = exposure_df.replace(0, np.nan).dropna(how='any', axis=1).columns.to_list()
-
-    l_col_info = get_loc_dtypes()
-    for k in list(l_col_info.keys()):
-        l_col_info[k.lower()] = l_col_info[k]
-        del l_col_info[k]
-
+    l_col_info = {k.lower(): v for k, v in get_loc_dtypes().items()}
     gul_avail = {k: l_col_info[k]['desc'] if k in l_col_info else desc_non_oed
                  for k in set([c.lower() for c in l_col_list]).difference(int_excluded_cols)}
-    gul_summary_lvl = {'GUL': {'available': {**gul_avail, **int_oasis_cols}}}
 
     # IL perspective (join of acc + loc col with no dups)
-    il_summary_lvl = {}
+    il_avail = {}
     if accounts_fp:
-        accounts_df = get_dataframe(accounts_fp, lowercase_cols=False)
+        accounts_df = get_dataframe(accounts_fp, lowercase_cols=True)
         a_col_list = accounts_df.loc[:, ~accounts_df.isnull().all()].columns.to_list()
-        a_col_info = get_acc_dtypes()
+        a_col_info = {k.lower(): v for k, v in get_acc_dtypes().items()}
         a_avail = set([c.lower() for c in a_col_list])
-
         il_avail = {k: a_col_info[k]['desc'] if k in a_col_info else desc_non_oed
                     for k in a_avail.difference(gul_avail.keys())}
-        il_summary_lvl = {'IL': {'available': {**gul_avail, **il_avail, **int_oasis_cols}}}
 
+    # Write JSON
+    gul_summary_lvl = {'GUL': {'available': {**gul_avail, **il_avail, **int_oasis_cols}}}
+    il_summary_lvl = {'IL': {'available': {**gul_avail, **il_avail, **int_oasis_cols}}} if il_avail else {}
     with io.open(os.path.join(target_dir, 'exposure_summary_levels.json'), 'w', encoding='utf-8') as f:
         f.write(json.dumps({**gul_summary_lvl, **il_summary_lvl}, sort_keys=True, ensure_ascii=False, indent=4))
 
