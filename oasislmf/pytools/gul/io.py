05d4b1667e67e7d1a59a7a29c3b268ea4ce6d9b4
mtazzari <mtazzari@gmail.com>
2022-10-25 15:13:53 +0100

    [gul] reverting mc modifications

--- oasislmf/pytools/gul/io.py
+++ oasislmf/pytools/gul/io.py
@@ -74,20 +74,10 @@ def read_getmodel_stream(stream_in, item_map, coverages, compute, seeds, buff_si
     # see https://github.com/OasisLMF/ktools/blob/master/docs/md/CoreComponents.md
     if stream_type[0] != 1:
         raise ValueError(f"FATAL: Invalid stream type: expect 1, got {stream_type[0]}.")
-    else:
-        monte_carlo = False
-
-        if stream_type[0] == 5:
-            monte_carlo = True
 
     # maximum number of entries is buff_size divided by the minimum entry size
     # (corresponding to a 1-bin only cdf)
-    if monte_carlo is True:
-        sample_size = np.frombuffer(stream_in.read(4), dtype='i4')
-
-        min_size_cdf_entry = 4 + 4 + sample_size * 4  # event_id, areaperil_id, haz intensity bin id samples...
-    else:
-        min_size_cdf_entry = damagecdfrec_stream.size + 4 + ProbMean.size
+    min_size_cdf_entry = damagecdfrec_stream.size + 4 + ProbMean.size
 
     # each record from getmodel stream is expected to contain:
     # 1 damagecdfrec_stream obj, 1 int (Nbins), a number `Nbins` of ProbMean objects
@@ -133,19 +123,11 @@ def read_getmodel_stream(stream_in, item_map, coverages, compute, seeds, buff_si
             break
 
         # read the streamed data into formatted data
-        if monte_carlo is True:
-            cursor, yield_event, event_id, rec, rec_idx_ptr, last_event_id, compute_i, items_data_i, items_data, rng_index, group_id_rng_index, damagecdf_i = stream_to_data_monte_carlo(
-                int32_mv, valid_buf, min_size_cdf_entry, last_event_id, item_map, coverages, valid_area_peril_dict,
-                compute_i, compute, items_data_i, items_data, seeds, rng_index, group_id_rng_index,
-                damagecdf_i, rec_idx_ptr
-            )
-
-        else:
-            cursor, yield_event, event_id, rec, rec_idx_ptr, last_event_id, compute_i, items_data_i, items_data, rng_index, group_id_rng_index, damagecdf_i = stream_to_data(
-                int32_mv, valid_buf, min_size_cdf_entry, last_event_id, item_map, coverages, valid_area_peril_dict,
-                compute_i, compute, items_data_i, items_data, seeds, rng_index, group_id_rng_index,
-                damagecdf_i, rec_idx_ptr
-            )
+        cursor, yield_event, event_id, rec, rec_idx_ptr, last_event_id, compute_i, items_data_i, items_data, rng_index, group_id_rng_index, damagecdf_i = stream_to_data(
+            int32_mv, valid_buf, min_size_cdf_entry, last_event_id, item_map, coverages, valid_area_peril_dict,
+            compute_i, compute, items_data_i, items_data, seeds, rng_index, group_id_rng_index,
+            damagecdf_i, rec_idx_ptr
+        )
 
         if cursor == 0 and len_read == 0:
             # here valid buff > 0, but not enough data to have a full item => the stream stopped prematurely
@@ -314,143 +296,6 @@ def stream_to_data(int32_mv, valid_buf, size_cdf_entry, last_event_id, item_map,
         damagecdf_i += 1
 
     return cursor, yield_event, event_id, rec[:rec_valid_len], rec_idx_ptr, last_event_id, compute_i, items_data_i, items_data, rng_index, group_id_rng_index, damagecdf_i
-
-
-@njit(cache=True, fastmath=True)
-def stream_to_data_monte_carlo(int32_mv, valid_buf, size_cdf_entry, last_event_id, item_map, coverages, valid_area_peril_dict,
-                               compute_i, compute, items_data_i, items_data, seeds, rng_index, group_id_rng_index, damagecdf_i, rec_idx_ptr, sample_size):
-    """Parse streamed data into data arrays.
-
-    Args:
-        int32_mv (ndarray): int32 view of the buffer
-        valid_buf (int): number of bytes with valid data
-        size_cdf_entry (int): size (in bytes) of a single record
-        last_event_id (int): event_id of the last event that was completed
-        item_map (Dict[ITEM_MAP_KEY_TYPE, ITEM_MAP_VALUE_TYPE]): dict storing
-          the mapping between areaperil_id, vulnerability_id to item.
-        coverages (numpy.ndarray[coverage_type]): array with coverage data.
-        compute_i (int): index of the last coverage id stored in `compute`.
-        compute (numpy.array[int]): list of coverage ids to be computed.
-        items_data_i (int): index of the last items_data_i stored in `items_data`.
-        items_data (numpy.array[items_data_type]): item-related data.
-        seeds (numpy.array[int]): the random seeds for each coverage_id.
-        rng_index (int): number of unique random seeds computed so far.
-        group_id_rng_index (Dict([int,int])): map of group ids to random seeds.
-        damagecdf_i (int): index of the last cdf record that has been read from stream and stored in `rec`.
-        rec_idx_ptr (numpy.array[int]): array with the indices of `rec` where each cdf record starts.
-
-    Returns:
-        int, bool, int, numpy.array[ProbMean], numpy.array[int], int, int, int, numpy.array[items_data_type],
-        int, Dict[ITEM_MAP_KEY_TYPE, ITEM_MAP_VALUE_TYPE]), int:
-          number of int numbers read from the int32_mv ndarray, whether the current event (id=`event_id`)
-          has been fully read, cdf record, array with the indices of `rec` where each cdf record starts, last or current
-          event id, index of the last coverage id stored in `compute`, index of the last items_data_i stored in `items_data`,
-          item-related data, number of unique random seeds computed so far, map of group ids to random seeds,
-          index of the last cdf record that has been read from stream and stored in `rec`W
-    """
-    yield_event = False
-
-    # `rec` is a temporary buffer to store the cdf being read
-    # conservative choice: size `rec` as if the entire buffer is filled with cdf bins
-    rec = np.zeros(valid_buf // ProbMean_size, dtype=ProbMean)
-
-    # int32 memoryview cursor
-    cursor = 0
-
-    # init a counter for the local `rec` array
-    last_rec_idx_ptr = 0
-    rec_valid_len = 0
-
-    while cursor * int32_mv.itemsize + size_cdf_entry <= valid_buf:
-
-        event_id, cursor = int32_mv[cursor], cursor + 1
-
-        if event_id != last_event_id:
-            # a new event has started
-            if last_event_id > 0:
-                # if this is not the beginning of the very first event, yield the event that was just completed
-                yield_event = True
-                cursor -= 1
-                break
-
-            last_event_id = event_id
-
-        areaperil_id, cursor = int32_mv[cursor:cursor +
-                                        areaperil_int_to_int32_size].view(areaperil_int)[0], cursor + areaperil_int_to_int32_size
-        # vulnerability_id, cursor = int32_mv[cursor], cursor + 1
-        # Nbins_to_read, cursor = int32_mv[cursor], cursor + 1
-        Nbins_to_read = sample_size
-
-        if cursor * int32_mv.itemsize + Nbins_to_read * ProbMean_size > valid_buf:
-            # if the next cdf record is not fully contained in the valid buf, then
-            # get more data in the buffer and put cursor back at the beginning of this cdf
-            cursor -= areaperil_int_to_int32_size + 1
-            break
-
-        # peril filter
-        if valid_area_peril_dict is not None:
-            if areaperil_id not in valid_area_peril_dict:
-                cursor += 2 * oasis_float_to_int32_size * Nbins_to_read
-                continue
-
-        # read damage cdf bins
-        start_rec = last_rec_idx_ptr
-        end_rec = start_rec + Nbins_to_read
-        for j in range(start_rec, end_rec, 1):
-            haz_int_bin_id, cursor = int32_mv[cursor], cursor + 1
-            # rec[j]['prob_to'][j] = vuln_array[, , haz_int_bin_id]
-            cursor += oasis_float_to_int32_size
-
-            rec[j]['bin_mean'] = int32_mv[cursor: cursor + oasis_float_to_int32_size].view(oasis_float)[0]
-            cursor += oasis_float_to_int32_size
-
-        rec_idx_ptr.append(rec_idx_ptr[-1] + Nbins_to_read)
-        last_rec_idx_ptr = end_rec
-        rec_valid_len += Nbins_to_read
-
-        # register the items to their coverage
-        item_key = tuple((areaperil_id, vulnerability_id))
-
-        for item in item_map[item_key]:
-            item_id, coverage_id, group_id = item
-
-            # if this group_id was not seen yet, process it.
-            # it assumes that hash only depends on event_id and group_id
-            # and that only 1 event_id is processed at a time.
-            if group_id not in group_id_rng_index:
-                group_id_rng_index[group_id] = rng_index
-                seeds[rng_index] = generate_hash(group_id, last_event_id)
-                this_rng_index = rng_index
-                rng_index += 1
-
-            else:
-                this_rng_index = group_id_rng_index[group_id]
-
-            coverage = coverages[coverage_id]
-            if coverage['cur_items'] == 0:
-                # no items were collected for this coverage yet: set up the structure
-                compute[compute_i], compute_i = coverage_id, compute_i + 1
-
-                while items_data.shape[0] < items_data_i + coverage['max_items']:
-                    # if items_data needs to be larger to store all the items, double it in size
-                    temp_items_data = np.empty(items_data.shape[0] * 2, dtype=items_data.dtype)
-                    temp_items_data[:items_data_i] = items_data[:items_data_i]
-                    items_data = temp_items_data
-
-                coverage['start_items'], items_data_i = items_data_i, items_data_i + coverage['max_items']
-
-            # append the data of this item
-            item_i = coverage['start_items'] + coverage['cur_items']
-            items_data[item_i]['item_id'] = item_id
-            items_data[item_i]['damagecdf_i'] = damagecdf_i
-            items_data[item_i]['rng_index'] = this_rng_index
-
-            coverage['cur_items'] += 1
-
-        damagecdf_i += 1
-
-    return cursor, yield_event, event_id, rec[:rec_valid_len], rec_idx_ptr, last_event_id, compute_i, items_data_i, items_data, rng_index, group_id_rng_index, damagecdf_i
-
 
 
 @njit(cache=True, fastmath=True)
