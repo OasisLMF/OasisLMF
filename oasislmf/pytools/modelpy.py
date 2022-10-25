238b8030619800a299cc7ca62b00459ec7e0a7d0
mtazzari <mtazzari@gmail.com>
2022-10-25 15:22:58 +0100

    [getmodel] Reverting unused mc modifications

--- oasislmf/pytools/modelpy.py
+++ oasislmf/pytools/modelpy.py
@@ -10,15 +10,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--file-in', help='names of the input file_path')
 parser.add_argument('-o', '--file-out', help='names of the output file_path')
 parser.add_argument('-r', '--run-dir', help='path to the run directory', default='.')
-parser.add_argument('--montecarlo',
-                    help='if passed, draw sample of the hazard intensity and stream them out (default: False)',
-                    action='store_true', dest='full_monte_carlo', default=False)
-parser.add_argument('-d', help='output random numbers instead of the sampled hazard intensity bins (default: False).',
-                    action='store_true', dest='debug', default=False)
-parser.add_argument('--random-generator',
-                    help='random number generator\n0: numpy default (MT19937), 1: Latin Hypercube. Default: 1.',
-                    default=1, type=int)
-parser.add_argument('-S', help='Sample size (default: 0).', default=0, action='store', type=int, dest='sample_size')
 parser.add_argument('--ignore-file-type', nargs='*', help='the type of file to be loaded', default=set())
 parser.add_argument('--data-server', help='=Use tcp/sockets for IPC data sharing', action='store_true')
 parser.add_argument('--peril-filter', help='Id of the peril to keep, if empty take all perils', nargs='+')
