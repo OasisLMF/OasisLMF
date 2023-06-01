93ff75ad320249a0d7ad0c6b73cd628bfc41068d
Sam Gamble <hexadessa@gmail.com>
2023-06-01 14:58:38 +0100

    Fix up generate_files and add tests

--- tests/computation/test_generate_files.py
+++ tests/computation/test_generate_files.py
@@ -20,7 +20,7 @@ class TestGenerateFiles(ComputationChecker):
         cls.manager = OasisManager()
 
         # Args
-        cls.default_args = cls.manager._params_generate_files()
+        cls.default_args = cls.manager._params_generate_oasis_files()
         cls.pre_hook_args = cls.manager._params_exposure_pre_analysis()
         cls.gen_files_args = cls.manager._params_generate_files()
 
@@ -32,15 +32,72 @@ class TestGenerateFiles(ComputationChecker):
         )
 
     def setUp(self):
-        import ipdb; ipdb.set_trace()
         self.min_args = {
             'oed_location_csv': self.tmp_files['oed_location_csv'].name,
-            'analysis_settings_json': self.tmp_files['analysis_settings_json'].name,
             'keys_data_csv': self.tmp_files['keys_data_csv'].name,
-            'model_data_dir': self.tmp_dirs['model_data_dir'].name
         }
-        self.write_json(self.tmp_files.get('analysis_settings_json'), MIN_RUN_SETTINGS)
         self.write_json(self.tmp_files.get('model_settings_json'), MIN_MODEL_SETTINGS)
         self.write_str(self.tmp_files.get('oed_location_csv'), MIN_LOC)
         self.write_str(self.tmp_files.get('oed_accounts_csv'), MIN_ACC)
         self.write_str(self.tmp_files.get('keys_data_csv'), MIN_KEYS)
+
+
+    def test_args__default_combine(self):
+        expt_combined_args = self.combine_args([
+            self.pre_hook_args,
+            self.gen_files_args,
+        ])
+        self.assertEqual(expt_combined_args, self.default_args)
+        
+
+    def test_generate__without_pre_analysis(self):
+        files_mock = MagicMock()
+        files_dir = self.tmp_dirs.get('oasis_files_dir').name
+        files_mock._get_output_dir.return_value = files_dir
+
+        call_args = self.min_args
+        with patch.object(oasislmf.computation.run.generate_files, 'GenerateFiles', files_mock): 
+            self.manager.generate_oasis_files(**call_args)
+
+        files_called_kwargs = self.called_args(files_mock)
+        expected_called_kwargs = self.combine_args([call_args, {"oasis_files_dir": files_dir}])
+
+        files_mock.assert_called_once()
+        self.assertEqual(files_called_kwargs, expected_called_kwargs)
+
+
+    def test_generate__with_pre_analysis(self):
+        pre_mock = MagicMock()
+        files_mock = MagicMock()
+        files_dir_obj = self.tmp_dirs.get('oasis_files_dir')
+        files_dir_obj.cleanup() # delete file to check if its created 
+
+        model_settings = self.tmp_files.get('model_settings_json').name
+        account = self.tmp_files.get('oed_accounts_csv').name
+
+        self.write_json(self.tmp_files.get('exposure_pre_analysis_setting_json'), {
+            "override_loc_num": 'test_loc_1',
+            "override_acc_num": 'test_acc_1',
+        })
+
+        call_args = self.combine_args([self.min_args, {
+            "model_settings_json": model_settings,
+            "oasis_files_dir": files_dir_obj.name,
+            "oed_accounts_csv": account,
+            "exposure_pre_analysis_module": FAKE_PRE_ANALYSIS_MODULE,
+            "exposure_pre_analysis_setting_json": self.tmp_files.get('exposure_pre_analysis_setting_json').name
+        }])
+
+        with patch.object(oasislmf.computation.run.generate_files, 'GenerateFiles', files_mock): 
+            self.manager.generate_oasis_files(**call_args)
+
+        exposure_data = files_mock.call_args.kwargs['exposure_data']
+        self.assertEqual(exposure_data.location.dataframe['LocNumber'][0], 'test_loc_1')
+        self.assertEqual(exposure_data.account.dataframe['AccNumber'][0], 'test_acc_1')
+
+        files_called_kwargs = self.called_args(files_mock)
+        expected_called_kwargs = self.combine_args([call_args])
+
+        files_mock.assert_called_once()
+        self.assertEqual(files_called_kwargs, expected_called_kwargs)
+        self.assertTrue(os.path.isdir(files_dir_obj.name))
