6d2cd6e09e8672965afa40d761bcb752c4d93611
mtazzari <mtazzari@gmail.com>
2023-08-02 10:54:59 +0100

    add GH workflow to build and deploy docs

---
+++ docs/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR_BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR_ENV="$(which python | xargs dirname)"
+
+# Update python env
+pip install -r requirements.txt
+
+# Update JSON specs 
+    # ./update-redoc.py
+
+# Build docs
+cd $DIR_BASE
+make html SPHINXBUILD="python ${DIR_ENV}/sphinx-build"
+
+# Create TAR
+# if [[ ! -d "$DIR_BASE/output/" ]]; then 
+#     mkdir $DIR_BASE/output/
+# fi 
+# tar -czvf OasisLMF_docs.tar.gz -C build/html/ .
+# mv OasisLMF_docs.tar.gz $DIR_BASE/output/
