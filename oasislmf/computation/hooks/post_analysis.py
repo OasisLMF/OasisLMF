12150feb1999ffd7da7f89e9464b2f6767f8da30
ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
2023-09-15 12:03:41 +0100

    added first test and basic functionality

--- oasislmf/computation/hooks/post_analysis.py
+++ oasislmf/computation/hooks/post_analysis.py
@@ -1,16 +1,46 @@
-# TO DO -- placeholder for post analysis output modification hook
+__all__ = [
+    'PostAnalysis'
+]
 
-# __all__ = [
-#    '_'
-# ]
-#
-#
-# class _(ComputationStep):
-#    """
-#    - desc here -
-#    """
-#    step_params = [  .. args ..  ]
-#
-#
-#    def run(self):
-#        return None
+import json
+import pathlib
+from pathlib import Path
+from ods_tools.oed import UnknownColumnSaveOption
+
+from ..base import ComputationStep
+from ...utils.data import get_exposure_data, prepare_location_df
+from ...utils.inputs import str2bool
+from ...utils.path import get_custom_module
+from ...utils.exceptions import OasisException
+
+
+class PostAnalysis(ComputationStep):
+    """
+    """
+    step_params = [{'name': 'post_analysis_module', 'required': True, 'is_path': True, 'pre_exist': True,
+                    'help': 'Post-Analysis module path'},
+                   {'name': 'post_analysis_class_name', 'default': 'PostAnalysis',
+                    'help': 'Name of the class to use for the post_analysis'},
+                    {'name': 'raw_output_dir', 'is_path': True, 'pre_exist': True, 'help': 'path to oasis output directory'},
+                    {'name': 'post_processed_output_dir', 'is_path': True, 'pre_exist': False, 'help': 'path to post-processed output directory'},]
+
+    run_dir_key = 'post-analysis'
+
+    def run(self):
+        _module = get_custom_module(self.post_analysis_module, 'Post-Analysis module path')
+        kwargs = dict()
+
+        kwargs['raw_output_dir'] = self.raw_output_dir
+        kwargs['post_processed_output_dir'] = self.post_processed_output_dir
+
+        Path(self.post_processed_output_dir).mkdir()
+
+        try:
+            _class = getattr(_module, self.post_analysis_class_name)
+        except AttributeError as e:
+            raise OasisException(f"class {self.post_analysis_class_name} "
+                                 f"is not defined in module {self.post_analysis_module}") from e.__cause__
+        
+        print(kwargs)
+        print(_class(**kwargs))
+        _class_return = _class(**kwargs).run()
