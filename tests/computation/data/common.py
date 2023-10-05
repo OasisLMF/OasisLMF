c9d3912422278028f1a14e22e6526c659af40cf3
Sam Gamble <hexadessa@gmail.com>
2023-10-05 18:06:08 +0100

    Quick and dirty bash script check

--- tests/computation/data/common.py
+++ tests/computation/data/common.py
@@ -5,6 +5,7 @@ __all__ = [
     'MIN_MODEL_SETTINGS',
     'INVALID_RUN_SETTINGS',
     'RI_AAL_SETTINGS',
+    'RI_ALL_ORD_OUTPUT_SETTINGS',
     'PARQUET_GUL_SETTINGS',
     'MIN_KEYS',
     'MIN_KEYS_ERR',
@@ -21,6 +22,7 @@ __all__ = [
     'GROUP_FIELDS_MODEL_SETTINGS',
     'OLD_GROUP_FIELDS_MODEL_SETTINGS',
     'merge_dirs',
+    'ALL_ORD_EXPECTED_SCRIPT',
 ]
 
 import os
@@ -157,6 +159,94 @@ RI_AAL_SETTINGS = {
 }
 
 
+RI_ALL_ORD_OUTPUT_SETTINGS = {
+    "model_supplier_id": "M-sup",
+    "model_name_id": 'M-name',
+    "model_settings": {},
+    "number_of_samples": 1,
+    "gul_output": True,
+    'return_periods': [1,10],
+    "model_settings": {
+    },
+    "gul_summaries": [
+        {
+            "id": 1,
+            "ord_output": {
+                "return_period_file": False,
+                "plt_sample": True,
+                "plt_quantile": True,
+                "plt_moment": True,
+                "elt_sample": True,
+                "elt_quantile": True,
+                "elt_moment": True,
+                "alt_period": True,
+                "alt_meanonly": True,
+                "ept_full_uncertainty_aep": True,
+                "ept_full_uncertainty_oep": True,
+                "ept_mean_sample_aep": True,
+                "ept_mean_sample_oep": True,
+                "ept_per_sample_mean_aep": True,
+                "ept_per_sample_mean_oep": True,
+                "psept_aep": True,
+                "psept_oep": True,
+                "parquet_format": True
+            }
+        }
+    ],
+    "il_output": True,
+    "il_summaries": [
+        {
+            "id": 1,
+            "ord_output": {
+                "plt_sample": True,
+                "plt_quantile": True,
+                "plt_moment": True,
+                "elt_sample": True,
+                "elt_quantile": True,
+                "elt_moment": True,
+                "alt_period": True,
+                "alt_meanonly": True,
+                "ept_full_uncertainty_aep": True,
+                "ept_full_uncertainty_oep": True,
+                "ept_mean_sample_aep": True,
+                "ept_mean_sample_oep": True,
+                "ept_per_sample_mean_aep": True,
+                "ept_per_sample_mean_oep": True,
+                "psept_aep": True,
+                "psept_oep": True,
+                "parquet_format": True,
+                "return_period_file": False
+            }
+        }
+    ],
+    "ri_output": True,
+    "ri_summaries": [
+        {
+            "id": 1,
+            "ord_output": {
+                "plt_sample": True,
+                "plt_quantile": True,
+                "plt_moment": True,
+                "elt_sample": True,
+                "elt_quantile": True,
+                "elt_moment": True,
+                "alt_period": True,
+                "alt_meanonly": True,
+                "ept_full_uncertainty_aep": True,
+                "ept_full_uncertainty_oep": True,
+                "ept_mean_sample_aep": True,
+                "ept_mean_sample_oep": True,
+                "ept_per_sample_mean_aep": True,
+                "ept_per_sample_mean_oep": True,
+                "psept_aep": True,
+                "psept_oep": True,
+                "parquet_format": True,
+                "return_period_file": False
+            }
+        }
+    ]
+}
+
 PARQUET_GUL_SETTINGS = {
     "model_supplier_id": "M-sup",
     "model_name_id": 'M-name',
@@ -235,3 +325,5 @@ FAKE_PRE_ANALYSIS_MODULE = os.path.join(os.path.dirname(__file__), 'fake_pre_ana
 
 FAKE_MODEL_RUNNER = os.path.join(os.path.dirname(__file__), 'fake_model_runner')
 FAKE_MODEL_RUNNER__OLD = os.path.join(os.path.dirname(__file__), 'fake_model_runner__old')
+
+ALL_ORD_EXPECTED_SCRIPT = os.path.join(os.path.dirname(__file__), 'ord_bash_script.sh')
