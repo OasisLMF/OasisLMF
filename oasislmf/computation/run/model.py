5e50504a684343f6a5c858109381dfc407ac94aa
sambles <sambles@users.noreply.github.com>
2023-11-07 15:40:44 +0000

    Backport - Post analysis hook (#1390)
    * Post analysis hook (#1371)
    
    * initial stage: creating tests
    
    * added first test and basic functionality
    
    * flake8 fixes and extended the test to read the file
    
    * Defaults
    
    * Rename var
    
    * Run postanalysis hook after model run
    
    * Fix
    
    * Simplify args
    
    * Pass through settings
    
    * Add test for settings
    
    * add test for wrong module name
    
    * Add model_data_dir to postanalysis hook
    
    * Rework tests
    
    * Simplify
    
    * initial stage: creating tests
    
    * added first test and basic functionality
    
    * flake8 fixes and extended the test to read the file
    
    * Defaults
    
    * Rename var
    
    * Run postanalysis hook after model run
    
    * Fix
    
    * Simplify args
    
    * Pass through settings
    
    * Add test for settings
    
    * add test for wrong module name
    
    * Add model_data_dir to postanalysis hook
    
    * Rework tests
    
    * Simplify
    
    * Make post_analysis_module a non required arg for model run cmd
    
    * Fix flake8
    
    * Add post_analysis to GenerateLosses command
    
    * Fix TestRunModel test
    
    * test
    
    * test plat fix
    
    * flake8
    
    * flake
    
    * Merge upstream fixes
    
    ---------
    
    Co-authored-by: ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
    Co-authored-by: Amit Afre <amit.afre@jbarisk.com>
    Co-authored-by: JonCrane <Jon.Crane@jbarisk.com>
    Co-authored-by: Sam Gamble <hexadessa@gmail.com>
    
    * Fix missing test files
    
    * pep
    
    * Drop py3.7 in testing and add py3.11 (#1280)
    
    * Drop py3.7 and add py3.11
    
    * Fix readme and upload-cov flag
    
    ---------
    
    Co-authored-by: carlfischerjba <carl.fischer@jbarisk.com>
    Co-authored-by: ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
    Co-authored-by: Amit Afre <amit.afre@jbarisk.com>
    Co-authored-by: JonCrane <Jon.Crane@jbarisk.com>

--- oasislmf/computation/run/model.py
+++ oasislmf/computation/run/model.py
@@ -12,6 +12,7 @@ from ..base import ComputationStep
 from ..generate.files import GenerateFiles
 from ..generate.losses import GenerateLosses
 from ..hooks.pre_analysis import ExposurePreAnalysis
+from ..hooks.post_analysis import PostAnalysis
 
 from ...utils.data import get_exposure_data
 from ...utils.path import empty_dir
@@ -28,12 +29,15 @@ class RunModel(ComputationStep):
             'help': 'Path to the directory in which to generate the Oasis files'},
         {'name': 'exposure_pre_analysis_module', 'required': False, 'is_path': True,
             'pre_exist': True, 'help': 'Exposure Pre-Analysis lookup module path'},
+        {'name': 'post_analysis_module', 'required': False, 'is_path': True, 'pre_exist': True,
+         'help': 'Post-Analysis module path'},
     ]
     # Add params from each sub command not in 'step_params'
     chained_commands = [
         GenerateLosses,
         GenerateFiles,
         ExposurePreAnalysis,
+        PostAnalysis,
     ]
 
     def get_exposure_data_config(self):
@@ -70,10 +74,12 @@ class RunModel(ComputationStep):
         self.kwargs['exposure_data'] = get_exposure_data(self, add_internal_col=True)
 
         # Run chain
+        cmds = []
         if self.exposure_pre_analysis_module:
-            cmds = [(ExposurePreAnalysis, self.kwargs), (GenerateFiles, self.kwargs), (GenerateLosses, self.kwargs)]
-        else:
-            cmds = [(GenerateFiles, self.kwargs), (GenerateLosses, self.kwargs)]
+            cmds += [(ExposurePreAnalysis, self.kwargs)]
+        cmds += [(GenerateFiles, self.kwargs), (GenerateLosses, self.kwargs)]
+        if self.post_analysis_module:
+            cmds += [(PostAnalysis, self.kwargs)]
 
         with tqdm(total=len(cmds)) as pbar:
             for cmd in cmds:
