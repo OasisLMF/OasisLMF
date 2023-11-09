aad56f71cf38495e00fac943103154137b58ccea
sambles <sambles@users.noreply.github.com>
2023-11-09 13:24:28 +0000

    Release 1.27.7 (#1395)
    * Set oasislmf to version 1.27.6
    
    * Backport - Post analysis hook (#1390)
    
    * Post analysis hook (#1371)
    
    * initial stage: creating tests
    
    * added first test and basic functionality
    
    * flake8 fixes and extended the test to read the file
    
    * Defaults
    
    * Rename var
    
    * Run postanalysis hook after model run
    
    * Fix
    
    * Simplify args
    
    * Pass through settings
    
    * Add test for settings
    
    * add test for wrong module name
    
    * Add model_data_dir to postanalysis hook
    
    * Rework tests
    
    * Simplify
    
    * initial stage: creating tests
    
    * added first test and basic functionality
    
    * flake8 fixes and extended the test to read the file
    
    * Defaults
    
    * Rename var
    
    * Run postanalysis hook after model run
    
    * Fix
    
    * Simplify args
    
    * Pass through settings
    
    * Add test for settings
    
    * add test for wrong module name
    
    * Add model_data_dir to postanalysis hook
    
    * Rework tests
    
    * Simplify
    
    * Make post_analysis_module a non required arg for model run cmd
    
    * Fix flake8
    
    * Add post_analysis to GenerateLosses command
    
    * Fix TestRunModel test
    
    * test
    
    * test plat fix
    
    * flake8
    
    * flake
    
    * Merge upstream fixes
    
    ---------
    
    Co-authored-by: ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
    Co-authored-by: Amit Afre <amit.afre@jbarisk.com>
    Co-authored-by: JonCrane <Jon.Crane@jbarisk.com>
    Co-authored-by: Sam Gamble <hexadessa@gmail.com>
    
    * Fix missing test files
    
    * pep
    
    * Drop py3.7 in testing and add py3.11 (#1280)
    
    * Drop py3.7 and add py3.11
    
    * Fix readme and upload-cov flag
    
    ---------
    
    Co-authored-by: carlfischerjba <carl.fischer@jbarisk.com>
    Co-authored-by: ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
    Co-authored-by: Amit Afre <amit.afre@jbarisk.com>
    Co-authored-by: JonCrane <Jon.Crane@jbarisk.com>
    
    * retest piwind
    
    * Fix/1219 flaky test failures (#1327)
    
    * replace any expected NaN with np.nan in assert_frame_equal
    
    * Fix
    
    * pep it
    
    * pep it
    
    * Remove deadline in lookup tests
    
    * Set oasislmf to version 1.27.7
    
    * test
    
    * Set Ktools to version 3.11.0
    
    * test
    
    * Add output zeros flag to summarycalc for all reinsurance loss computes (#1397)
    
    * Add output zeros flag to summarycalc for all reinsurance loss computes
    
    * Update summarycalc flags
    
    * retest
    
    * Update changelog
    
    ---------
    
    Co-authored-by: awsbuild <awsbuild@oasislmf.org>
    Co-authored-by: carlfischerjba <carl.fischer@jbarisk.com>
    Co-authored-by: ksenija.vasiljeva <ksenija.vasiljeva@jbarisk.com>
    Co-authored-by: Amit Afre <amit.afre@jbarisk.com>
    Co-authored-by: JonCrane <Jon.Crane@jbarisk.com>
    Co-authored-by: hchagani-oasislmf <47981469+hchagani-oasislmf@users.noreply.github.com>

--- oasislmf/manager.py
+++ oasislmf/manager.py
@@ -19,8 +19,10 @@ from oasislmf.computation.helper.autocomplete import HelperTabComplete
 from oasislmf.computation.helper.cookiecutter import (CreateComplexModelRepo,
                                                       CreateModelRepo)
 from oasislmf.computation.hooks.pre_analysis import ExposurePreAnalysis
+from oasislmf.computation.hooks.post_analysis import PostAnalysis
 from oasislmf.computation.run.exposure import RunExposure, RunFmTest
 from oasislmf.computation.run.generate_files import GenerateOasisFiles
+from oasislmf.computation.run.generate_losses import GenerateOasisLosses
 from oasislmf.computation.run.model import RunModel
 from oasislmf.computation.run.platform import (PlatformDelete, PlatformGet,
                                                PlatformList, PlatformRun,
@@ -34,6 +36,7 @@ class OasisManager(object):
         ExposurePreAnalysis,
         GenerateFiles,
         GenerateOasisFiles,
+        GenerateOasisLosses,
         GenerateKeys,
         GenerateKeysDeterministic,
 
@@ -47,6 +50,7 @@ class OasisManager(object):
         GenerateDummyModelFiles,
         GenerateDummyOasisFiles,
         RunModel,
+        PostAnalysis,
         RunExposure,
         RunFmTest,
         PlatformList,
