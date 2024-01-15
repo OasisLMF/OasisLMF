4c89c3cb5f00fea88fba46052d9773a5ab30ad6a
sambles <sambles@users.noreply.github.com>
2024-01-15 11:01:15 +0000

    collect_unused_df in il preparation (#1340) (#1424)
    Co-authored-by: Stephane Struzik <34556337+sstruzik@users.noreply.github.com>

--- oasislmf/preparation/gul_inputs.py
+++ oasislmf/preparation/gul_inputs.py
@@ -224,6 +224,9 @@ def get_gul_input_items(
                 correlation_check = True
 
     query_nonzero_tiv = " | ".join(f"({tiv_col} != 0)" for tiv_col in tiv_cols)
+    for tiv_col in tiv_cols:
+        if tiv_col not in exposure_df.columns:
+            exposure_df[tiv_col] = 0
     exposure_df.loc[:, tiv_cols] = exposure_df.loc[:, tiv_cols].fillna(0.0)
     exposure_df.query(query_nonzero_tiv, inplace=True, engine='numexpr')
 
