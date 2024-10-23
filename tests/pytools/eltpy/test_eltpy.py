0158f7d611231cc60dfd34076c23f83282372172
SkylordA <anish.kothikar@oasislmf.org>
2024-10-23 15:01:10 +0100

    modifies gitignore to account for test_eltpy binary files

--- tests/pytools/eltpy/test_eltpy.py
+++ tests/pytools/eltpy/test_eltpy.py
@@ -5,3 +5,5 @@ from tempfile import TemporaryDirectory
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_eltpy")
 
+def test_read_quantile_get_intervals():
+    pass
