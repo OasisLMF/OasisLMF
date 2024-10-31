056502b50d5c7b1adec95a21766ba666b931eb80
Stephane Struzik <34556337+sstruzik@users.noreply.github.com>
2024-10-31 15:38:29 +0000

    generate hazard_group_id even if no correlation (#1584)

--- tests/preparation/test_correlations.py
+++ tests/preparation/test_correlations.py
@@ -4,10 +4,7 @@ This file tests the mapping of the correlation data between supported perils and
 import os
 from unittest import TestCase, main
 
-import pandas as pd
-
-from oasislmf.preparation.correlations import (get_correlation_input_items,
-                                               map_data)
+from oasislmf.preparation.correlations import map_data
 from ods_tools.oed.setting_schema import ModelSettingSchema
 
 META_PATH = os.path.realpath(__file__).replace("test_correlations.py", "meta_data/")
@@ -23,36 +20,25 @@ class TestMapData(TestCase):
         pass
 
     def test_map_data(self):
+        expected_mapped_data = [
+            {
+                'id': 'WSS',
+                'desc': 'Single Peril: Storm Surge',
+                'peril_correlation_group': 1,
+                'damage_correlation_value': '0.7',
+                'hazard_correlation_value': '0.0',
+            },
+            {
+                'id': 'WTC',
+                'desc': 'Single Peril: Tropical Cyclone',
+                'peril_correlation_group': 2,
+                'damage_correlation_value': '0.5',
+                'hazard_correlation_value': '0.3',
+            },
+        ]
+
         mapped_data = map_data(data=self.model_settings, logger=None)
-        self.assertEqual(EXPECTED_MAPPED_DATA, mapped_data.to_dict('records'))
-
-    def test_get_correlation_input_items(self):
-        gul_path = META_PATH + "gul_inputs_df.csv"
-
-        gul_inputs_df = pd.read_csv(gul_path)
-        correlation_df = get_correlation_input_items(correlation_map_df=map_data(data=self.model_settings, logger=None),
-                                                     gul_inputs_df=gul_inputs_df)
-        correlation_df_check = pd.read_csv(f"{META_PATH}correlation_df.csv")
-
-        correlation_df_check.equals(correlation_df)
-
-
-EXPECTED_MAPPED_DATA = [
-    {
-        'id': 'WSS',
-        'desc': 'Single Peril: Storm Surge',
-        'peril_correlation_group': 1,
-        'damage_correlation_value': '0.7',
-        'hazard_correlation_value': '0.0',
-    },
-    {
-        'id': 'WTC',
-        'desc': 'Single Peril: Tropical Cyclone',
-        'peril_correlation_group': 2,
-        'damage_correlation_value': '0.5',
-        'hazard_correlation_value': '0.3',
-    },
-]
+        self.assertEqual(expected_mapped_data, mapped_data.to_dict('records'))
 
 
 if __name__ == "__main__":
