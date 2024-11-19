4cd7cafc1a8f68b0d6263719224c0e29f49ec544
SkylordA <anish.kothikar@oasislmf.org>
2024-11-19 14:32:58 +0000

    implements read_input_files functions and test

--- oasislmf/pytools/aal/manager.py
+++ oasislmf/pytools/aal/manager.py
@@ -3,6 +3,8 @@
 import logging
 import numpy as np
 import numba as nb
+import struct
+from pathlib import Path
 
 from oasislmf.pytools.common.data import (oasis_int, oasis_float, oasis_int_size, oasis_float_size)
 from oasislmf.pytools.common.event_stream import (EventReader, mv_read)
@@ -77,6 +79,172 @@ def read_buffer(
 
     # Update the indices
     return cursor, event_id, item_id, 0
+
+
+def read_occurrence(occurrence_fp):
+    """Read the occurrence binary file and returns an occurrence map
+    Args:
+        occurrence_fp (str | os.PathLike): Path to the occurrence binary file
+    Returns:
+        occ_map (ndarray[occ_map_dtype]): numpy map of event_id, period_no, occ_date_id from the occurrence file
+    """
+    try:
+        with open(occurrence_fp, "rb") as fin:
+            # Extract Date Options
+            date_opts = fin.read(4)
+            if not date_opts or len(date_opts) < 4:
+                error_msg = "Occurrence file is empty or currupted"
+                logger.error(error_msg)
+                raise RuntimeError(error_msg)
+            date_opts = int.from_bytes(date_opts, byteorder="little", signed=True)
+
+            date_algorithm = date_opts & 1  # Unused as granular_date not supported
+            granular_date = date_opts >> 1
+
+            # (event_id: int, period_no: int, occ_date_id: int)
+            record_format = "<iii"
+            # (event_id: int, period_no: int, occ_date_id: long long)
+            if granular_date:
+                record_format = "<iiq"
+            record_size = struct.calcsize(record_format)
+
+            # Should not get here
+            if not date_algorithm and granular_date:
+                error_msg = "FATAL: Unknown date algorithm"
+                logger.error(error_msg)
+                raise RuntimeError(error_msg)
+
+            # Extract no_of_periods
+            no_of_periods = fin.read(4)
+            if not no_of_periods or len(no_of_periods) < 4:
+                error_msg = "Occurrence file is empty or currupted"
+                logger.error(error_msg)
+                raise RuntimeError(error_msg)
+            no_of_periods = int.from_bytes(no_of_periods, byteorder="little", signed=True)
+
+            data = fin.read()
+
+        num_records = len(data) // record_size
+        if len(data) % record_size != 0:
+            logger.warning(
+                f"Occurrence File size (num_records: {num_records}) does not align with expected record size (record_size: {record_size})"
+            )
+
+        occ_map_dtype = np.dtype([
+            ("event_id", np.int32),
+            ("period_no", np.int32),
+            ("occ_date_id", np.int32),
+        ])
+        if granular_date:
+            occ_map_dtype = np.dtype([
+                ("event_id", np.int32),
+                ("period_no", np.int32),
+                ("occ_date_id", np.int64),
+            ])
+
+        occ_map = np.zeros(num_records, dtype=occ_map_dtype)
+
+        for i in range(num_records):
+            offset = i * record_size
+            curr_data = data[offset:offset + record_size]
+            if len(curr_data) < record_size:
+                break
+            event_id, period_no, occ_date_id = struct.unpack(record_format, curr_data)
+            occ_map[i] = (event_id, period_no, occ_date_id)
+
+        return occ_map, date_algorithm, granular_date, no_of_periods
+    except FileNotFoundError:
+        error_msg = f"FATAL: Error opening file {occurrence_fp}"
+        logger.error(error_msg)
+        raise FileNotFoundError(error_msg)
+    except Exception as e:
+        error_msg = f"An error occurred: {str(e)}"
+        logger.error(error_msg)
+        raise RuntimeError(error_msg)
+
+
+def read_periods(periods_fp, no_of_periods):
+    """Returns an array of period weights for each period between 1 and no_of_periods inclusive (with no gaps).
+    Args:
+        periods_fp (str | os.PathLike): Path to periods binary file
+        no_of_periods (int): Number of periods
+    Returns:
+        period_weights (ndarray[period_weights_dtype]): Returns the period weights
+    """
+    period_weights_dtype = np.dtype([
+        ("period_no", np.int32),
+        ("weighting", np.float64),
+    ])
+
+    period_weights = np.zeros(no_of_periods, dtype=period_weights_dtype)
+
+    try:
+        with open(periods_fp, "rb") as fin:
+            record_format = "<id"  # int, double
+            record_size = struct.calcsize(record_format)
+            num_read = 0
+            while True:
+                record_data = fin.read(record_size)
+
+                if not record_data:
+                    break
+
+                period_no, weighting = struct.unpack(record_format, record_data)
+
+                # Checks for gaps in periods
+                if num_read + 1 != period_no:
+                    error_msg = f"ERROR: Missing period_no in period binary file {periods_fp}."
+                    logger.error(error_msg)
+                    raise RuntimeError(error_msg)
+                num_read += 1
+
+                # More data than no_of_periods
+                if num_read > no_of_periods:
+                    error_msg = f"ERROR: no_of_periods does not match total period_no in period binary file {periods_fp}."
+                    logger.error(error_msg)
+                    raise RuntimeError(error_msg)
+
+                period_weights[period_no - 1] = (period_no, weighting)
+
+            # Less data than no_of_periods
+            if num_read != no_of_periods:
+                error_msg = f"ERROR: no_of_periods does not match total period_no in period binary file {periods_fp}."
+                logger.error(error_msg)
+                raise RuntimeError(error_msg)
+    except FileNotFoundError:
+        # If no periods binary file found, the revert to using period weights reciprocal to no_of_periods
+        logger.warning(f"Periods file not found at {periods_fp}, using reciprocal calculated period weights based on no_of_periods {no_of_periods}")
+        period_weights = np.array(
+            [(i + 1, 1 / no_of_periods) for i in range(no_of_periods)],
+            dtype=period_weights_dtype
+        )
+        return period_weights
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        raise RuntimeError(f"An error occurred: {str(e)}")
+
+    return period_weights
+
+
+def read_input_files(run_dir, sample_size):
+    """Reads all input files and returns a dict of relevant data
+    Args:
+        run_dir (str | os.PathLike): Path to directory containing required files structure
+        sample_size (int): Sample size
+    Returns:
+        file_data (Dict[str, Any]): A dict of relevent data extracted from files
+    """
+    occ_map, date_algorithm, granular_date, no_of_periods = read_occurrence(Path(run_dir, "input", "occurrence.bin"))
+    period_weights = read_periods(Path(run_dir, "input", "periods.bin"), no_of_periods)
+
+    file_data = {
+        "occ_map": occ_map,
+        "date_algorithm": date_algorithm,
+        "granular_date": granular_date,
+        "no_of_periods": no_of_periods,
+        "period_weights": period_weights,
+    }
+    return file_data
 
 
 def run(run_dir, subfolder, aal=None, alct=None, meanonly=False):
--- tests/pytools/aalpy/test_aalpy.py
+++ tests/pytools/aalpy/test_aalpy.py
@@ -1,16 +1,122 @@
 
 # import filecmp
-# import numpy as np
-# import pytest
+import numpy as np
+import pytest
 # import shutil
 from pathlib import Path
 # from tempfile import TemporaryDirectory
 
 # from oasislmf.pytools.common.data import (oasis_int, oasis_float)
-# from oasislmf.pytools.aal.manager import main
+from oasislmf.pytools.aal.manager import read_occurrence, read_periods
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_aalpy")
 
 
-def dummy_test():
-    pass
+def test_read_occurrence():
+    """Tests read_occurrence non granular
+    """
+    occurrence_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "occurrence.bin")
+    occ_map_dtype = np.dtype([
+        ("event_id", np.int32),
+        ("period_no", np.int32),
+        ("occ_date_id", np.int32),
+    ])
+
+    occ_map, date_algorithm, granular_date, no_of_periods = read_occurrence(occurrence_fp)
+    assert isinstance(occ_map, np.ndarray)
+    assert occ_map.dtype == occ_map_dtype
+    assert date_algorithm == True
+    assert granular_date == False
+    assert no_of_periods == 9
+
+
+def test_read_occurrence_granular():
+    """Tests read_occurrence granular
+    """
+    occurrence_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "occurrence_gran.bin")
+    occ_map_dtype = np.dtype([
+        ("event_id", np.int32),
+        ("period_no", np.int32),
+        ("occ_date_id", np.int64),
+    ])
+
+    occ_map, date_algorithm, granular_date, no_of_periods = read_occurrence(occurrence_fp)
+    assert isinstance(occ_map, np.ndarray)
+    assert occ_map.dtype == occ_map_dtype
+    assert date_algorithm == True
+    assert granular_date == True
+    assert no_of_periods == 9
+
+
+def test_read_periods():
+    """Tests read_periods from existing binary file
+    """
+    periods_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "periods.bin")
+    no_of_periods = 5
+    period_weights_dtype = np.dtype([
+        ("period_no", np.int32),
+        ("weighting", np.float64),
+    ])
+
+    periods_expected = np.array(
+        [(1, 0.15), (2, 0.05), (3, 0.4), (4, 0.1), (5, 0.3)],
+        dtype=period_weights_dtype
+    )
+    periods_actual = read_periods(periods_fp, no_of_periods)
+
+    period_no_expected = periods_expected[:]["period_no"]
+    weighting_expected = periods_expected[:]["weighting"]
+    period_no_actual = periods_actual[:]["period_no"]
+    weighting_actual = periods_actual[:]["weighting"]
+
+    np.testing.assert_array_almost_equal(period_no_expected, period_no_actual, decimal=3, verbose=True)
+    np.testing.assert_array_almost_equal(weighting_expected, weighting_actual, decimal=3, verbose=True)
+
+
+def test_read_periods_no_file():
+    """Tests read_periods with missing periods file, generates weights reciprocal of period_no
+    """
+    periods_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "periods_doesnotexist.bin")
+    no_of_periods = 5
+
+    period_weights_dtype = np.dtype([
+        ("period_no", np.int32),
+        ("weighting", np.float64),
+    ])
+
+    periods_expected = np.array(
+        [(1, 0.2), (2, 0.2), (3, 0.2), (4, 0.2), (5, 0.2)],
+        dtype=period_weights_dtype
+    )
+    periods_actual = read_periods(periods_fp, no_of_periods)
+
+    period_no_expected = periods_expected[:]["period_no"]
+    weighting_expected = periods_expected[:]["weighting"]
+    period_no_actual = periods_actual[:]["period_no"]
+    weighting_actual = periods_actual[:]["weighting"]
+
+    np.testing.assert_array_almost_equal(period_no_expected, period_no_actual, decimal=3, verbose=True)
+    np.testing.assert_array_almost_equal(weighting_expected, weighting_actual, decimal=3, verbose=True)
+
+
+def test_read_periods_missing_period():
+    """Tests read_periods with missing period_no in original binary file
+    """
+    periods_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "periods_missing.bin")
+    no_of_periods = 5
+
+    with pytest.raises(RuntimeError, match="Missing period_no"):
+        read_periods(periods_fp, no_of_periods)
+
+
+def test_read_periods_wrong_period_no():
+    """Tests read_periods with incorrect period_no compared to binary file
+    """
+    periods_fp = Path(TESTS_ASSETS_DIR, "input_file_tests", "periods.bin")
+    no_of_periods = 5
+
+    with pytest.raises(RuntimeError, match="no_of_periods does not match total period_no"):
+        read_periods(periods_fp, no_of_periods - 1)
+
+    with pytest.raises(RuntimeError, match="no_of_periods does not match total period_no"):
+        read_periods(periods_fp, no_of_periods + 1)
