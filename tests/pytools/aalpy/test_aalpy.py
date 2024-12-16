ada60abe0de481d05bf008a03ff7deb278adc56a
SkylordA <anish.kothikar@oasislmf.org>
2024-12-16 14:51:45 +0000

    remove redundant imports

--- tests/pytools/aalpy/test_aalpy.py
+++ tests/pytools/aalpy/test_aalpy.py
@@ -1,13 +1,11 @@
 
 import filecmp
-import numpy as np
-import pytest
 import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 # from oasislmf.pytools.common.data import (oasis_int, oasis_float)
-from oasislmf.pytools.aal.manager import main, read_occurrence, read_periods
+from oasislmf.pytools.aal.manager import main
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_aalpy")
 
