2f42e408427096e36d9d954c984388833178ee38
SkylordA <anish.kothikar@oasislmf.org>
2025-01-07 12:17:52 +0000

    removes commented import

--- tests/pytools/aalpy/test_aalpy.py
+++ tests/pytools/aalpy/test_aalpy.py
@@ -4,7 +4,6 @@ import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-# from oasislmf.pytools.common.data import (oasis_int, oasis_float)
 from oasislmf.pytools.aal.manager import main
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_aalpy")
