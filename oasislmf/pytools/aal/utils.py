ba174e2c93834261669933324e1022e44791e56f
Stephane Struzik <34556337+sstruzik@users.noreply.github.com>
2025-01-17 14:17:45 +0000

    improve aal index search (#1621)
    * improve aal index search

--- oasislmf/pytools/aal/utils.py
+++ oasislmf/pytools/aal/utils.py
@@ -110,3 +110,33 @@ def heap_pop(heap, size):
 def init_heap(num_rows=4):
     """Initialise heap"""
     return np.zeros((num_rows, 5), dtype=np.int32)
+
+
+@nb.njit(cache=True, error_model="numpy")
+def exact_binary_search(arr, value):
+    """
+    if no match is found return len(arr) instead of the insert index like in normal binary search
+    >>> exact_binary_search(np.array([1,3,5,7,9]), 3)
+    1
+
+    >>> exact_binary_search(np.array([1,3,5,7,9]), 0)
+    5
+
+    >>> exact_binary_search(np.array([1,3,5,7,9]), 4)
+    5
+
+    >>> exact_binary_search(np.array([1,3,5,7,9]), 9)
+    4
+    """
+    l = 0
+    r = len(arr)
+    while l < r:
+        mid = (l + r) // 2
+        if arr[mid] < value:
+            l = mid + 1
+        else:
+            r = mid
+    if l < len(arr) and value != arr[l]:
+        return len(arr)
+    else:
+        return l
