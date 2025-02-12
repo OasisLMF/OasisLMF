9e1322f087e34fb4950f0f51b353b4eede0160d9
SkylordA <anish.kothikar@oasislmf.org>
2025-02-12 07:18:11 +0000

    update aalpy test to account for floating point error

--- tests/pytools/aalpy/test_aalpy.py
+++ tests/pytools/aalpy/test_aalpy.py
@@ -4,6 +4,7 @@ import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+import numpy as np
 from oasislmf.pytools.aal.manager import main
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_aalpy")
@@ -45,18 +46,23 @@ def case_runner(sub_folder, test_name, meanonly=False):
         if test_name in ["aal", "alct"]:
             kwargs[f"{test_name}"] = actual_csv
         else:
-            raise Exception(f"Invalid or unimplemented test case {test_name} for pltpy")
+            raise Exception(f"Invalid or unimplemented test case {test_name} for aalpy")
 
         main(**kwargs)
 
         try:
-            assert filecmp.cmp(expected_csv, actual_csv, shallow=False)
+            expected_csv_data = np.genfromtxt(expected_csv, delimiter=',', skip_header=1)
+            actual_csv_data = np.genfromtxt(actual_csv, delimiter=',', skip_header=1)
+            if expected_csv_data.shape != actual_csv_data.shape:
+                raise AssertionError(
+                    f"Shape mismatch: {expected_csv} has shape {expected_csv_data.shape}, {actual_csv} has shape {actual_csv_data.shape}")
+            np.testing.assert_allclose(expected_csv_data, actual_csv_data, rtol=1e-5, atol=1e-8)
         except Exception as e:
             error_path = Path(TESTS_ASSETS_DIR, "all_files", "error_files")
             error_path.mkdir(exist_ok=True)
             shutil.copyfile(actual_csv, Path(error_path, csv_name))
             arg_str = ' '.join([f"{k}={v}" for k, v in kwargs.items()])
-            raise Exception(f"running 'pltpy {arg_str}' led to diff, see files at {error_path}") from e
+            raise Exception(f"running 'aalpy {arg_str}' led to diff, see files at {error_path}") from e
 
 
 def test_aal_output():
