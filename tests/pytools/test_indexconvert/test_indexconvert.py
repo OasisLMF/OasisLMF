1747fc6797447d3489be54de53e47ba673182ad0
Ha-Ree <harrys_spam@rocketship.com>
2025-02-21 10:53:47 +0000

    PEP8 Guidelines

--- tests/pytools/test_indexconvert/test_indexconvert.py
+++ tests/pytools/test_indexconvert/test_indexconvert.py
@@ -1,32 +1,38 @@
 import shutil
 import filecmp
 import os
-import pytest
-from oasislmf.pytools.indexconvert import change_footprint_apid_multi_peril, change_footprint_apid
+from oasislmf.pytools.indexconvert import (
+    change_footprint_apid_multi_peril, change_footprint_apid
+)
+
 
 def test_indexconvert():
     test_dir = os.path.dirname(__file__)
     input_file = os.path.join(test_dir, "indexconvert_test_data.csv")
     temp_file = os.path.join(test_dir, "indexconvert_test_temp.csv")
-    correct_file = os.path.join(test_dir, "indexconvert_test_correct1peril.csv")
-    
+    correct_file = os.path.join(
+        test_dir, "indexconvert_test_correct1peril.csv"
+    )
+
     assert os.path.exists(input_file)
     shutil.copy(input_file, temp_file)
     change_footprint_apid(temp_file, 10)
     assert filecmp.cmp(temp_file, correct_file, shallow=False)
-    
+
     os.remove(temp_file)
-    
+
 
 def test_indexconvert_multiperil():
     test_dir = os.path.dirname(__file__)
     input_file = os.path.join(test_dir, "indexconvert_test_data.csv")
     temp_file = os.path.join(test_dir, "indexconvert_test_temp.csv")
-    correct_file = os.path.join(test_dir, "indexconvert_test_correct3peril.csv")
-    
+    correct_file = os.path.join(
+        test_dir, "indexconvert_test_correct3peril.csv"
+    )
+
     assert os.path.exists(input_file)
     shutil.copy(input_file, temp_file)
     change_footprint_apid_multi_peril(temp_file, 10, 10, 3)
     assert filecmp.cmp(temp_file, correct_file, shallow=False)
-    
+
     os.remove(temp_file)
