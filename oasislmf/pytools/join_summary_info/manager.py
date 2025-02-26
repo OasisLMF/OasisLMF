9157f0f71c561912dcdbc9d3910835bba64638a0
SkylordA <anish.kothikar@oasislmf.org>
2025-02-26 13:09:29 +0000

    implements join-summary-info and tests

---
+++ oasislmf/pytools/join_summary_info/manager.py
@@ -0,0 +1,88 @@
+# join-summary-info/manager.py
+
+import logging
+import numpy as np
+from contextlib import ExitStack
+
+from oasislmf.pytools.utils import redirect_logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def load_summary_info(stack, summaryinfo_file):
+    """Load summary-info data into an array as strings to maintain sigfigs/formatting
+    Args:
+        stack (ExitStack): Exit Stack
+        summaryinfo_file (str | os.PathLike): Path to summary-info csv file
+    Returns:
+        full_summary_data (ndarray[object]): Array of strings, indexed by Summary Id
+        headers (List[str]): List of strings for summary info headers to add to data
+        max_summary_id (int): Max Summary ID
+    """
+    summary_ids = []
+    summary_data = []
+
+    with stack.enter_context(open(summaryinfo_file, "r")) as fin:
+        headers = fin.readline().strip().split(",")
+        summary_id_col_idx = headers.index("summary_id")
+
+        for line in fin:
+            row = line.strip().split(",")
+            summary_ids.append(row[summary_id_col_idx])
+            summary_data.append(",".join(row[:summary_id_col_idx] + row[summary_id_col_idx + 1:]))
+
+    headers = headers[:summary_id_col_idx] + headers[summary_id_col_idx + 1:]
+
+    summary_ids = np.array(summary_ids, dtype=np.int64)
+    summary_data = np.array(summary_data, dtype=object)
+
+    max_summary_id = summary_ids.max()
+
+    full_summary_data = np.full((max_summary_id + 1,), "," * (len(headers) - 1), dtype=object)
+    for i in range(len(summary_ids)):
+        full_summary_data[summary_ids[i]] = summary_data[i]
+
+    return full_summary_data, headers, max_summary_id
+
+
+def run(
+    summaryinfo_file,
+    data_file,
+    output_file,
+):
+    """Join the summary-info file to the given ORD data file based on SummaryId
+    Args:
+        summaryinfo_file (str | os.PathLike): Path to summary-info csv file
+        data_file (str | os.PathLike): Path to ORD output data file (e.g. SELT, MPLT, AAL, PSEPT) 
+        output_file (str | os.PathLike): Path to combined output file
+    """
+    with ExitStack() as stack:
+        summary_data, summary_headers, max_summary_id = load_summary_info(stack, summaryinfo_file)
+
+        with stack.enter_context(open(data_file, "r")) as data_fin, stack.enter_context(open(output_file, "w")) as fout:
+            data_headers = data_fin.readline().strip().split(",")
+            fout.write(",".join(data_headers + summary_headers) + "\n")
+
+            summary_id_col_idx = data_headers.index("SummaryId")
+            for line in data_fin:
+                row = line.strip().split(",")
+                summary_id = int(row[summary_id_col_idx])
+                if summary_id > max_summary_id:
+                    fout.write(line.strip() + ("," * len(summary_headers)) + "\n")
+                else:
+                    fout.write(line.strip() + "," + summary_data[summary_id] + "\n")
+
+
+@redirect_logging(exec_name='join-summary-info')
+def main(
+    summaryinfo=None,
+    data=None,
+    output=None,
+    **kwargs
+):
+    run(
+        summaryinfo_file=summaryinfo,
+        data_file=data,
+        output_file=output,
+    )
--- tests/pytools/join-summary-info/test_join-summary-info.py
+++ tests/pytools/join-summary-info/test_join-summary-info.py
@@ -1,15 +1,52 @@
+import filecmp
+import pytest
+import shutil
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from oasislmf.pytools.join_summary_info.manager import main
 
 TESTS_ASSETS_DIR = Path(__file__).parent.parent.parent.joinpath("assets").joinpath("test_join-summary-info")
 
 
 def test_join():
-    """Tests EPT output
+    """Tests join-summary-info output with ORD file
     """
-    pass
+    csv_summaryinfo = Path(TESTS_ASSETS_DIR, "gul_summary-info.csv")
+    csv_data = Path(TESTS_ASSETS_DIR, "aalgul_ord.csv")
+    csv_expected = Path(TESTS_ASSETS_DIR, "joined_aalgul_ord.csv")
+    with TemporaryDirectory() as tmp_result_dir_str:
+        csv_actual = Path(tmp_result_dir_str, "joined_aalgul_ord.csv")
+        kwargs = {
+            "summaryinfo": csv_summaryinfo,
+            "data": csv_data,
+            "output": csv_actual,
+        }
+
+        main(**kwargs)
+        error_path = Path(TESTS_ASSETS_DIR, "error_files")
+        arg_str = ' '.join([f"{k}={v}" for k, v in kwargs.items()])
+        try:
+            assert filecmp.cmp(csv_expected, csv_actual, shallow=False)
+        except Exception as e:
+            error_path.mkdir(exist_ok=True)
+            shutil.copyfile(Path(csv_actual),
+                            Path(error_path, "joined_aalgul_ord.csv"))
+            raise Exception(f"running 'join-summary-info {arg_str}' led to diff, see files at {error_path}") from e
 
 
 def test_missing_summary_col():
-    """Tests PSEPT output
+    """Tests join-summary-info with non-ORD file, should not generate output
     """
-    pass
+    csv_summaryinfo = Path(TESTS_ASSETS_DIR, "gul_summary-info.csv")
+    csv_data = Path(TESTS_ASSETS_DIR, "aalgul_nonord.csv")
+    with TemporaryDirectory() as tmp_result_dir_str:
+        csv_actual = Path(tmp_result_dir_str, "joined_aalgul_ord.csv")
+        kwargs = {
+            "summaryinfo": csv_summaryinfo,
+            "data": csv_data,
+            "output": csv_actual,
+        }
+
+        with pytest.raises(ValueError, match="\'SummaryId\' is not in list"):
+            main(**kwargs)
