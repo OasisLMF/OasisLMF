210711caf6a63af92c6b62d9268df7990de6220d
sambles <sambles@users.noreply.github.com>
2025-03-13 14:58:31 +0000

    Fix workaround of "less than good" tests (#1652)

--- tests/utils/test_data.py
+++ tests/utils/test_data.py
@@ -27,6 +27,13 @@ from oasislmf.utils.data import (PANDAS_DEFAULT_NULL_VALUES, factorize_array,
 from oasislmf.utils.exceptions import OasisException
 
 
+# Excluded all "nan" Strings as it breaks the testing
+#
+#  This is a workaround for fragile tests, these are generally not very good or meaningful and should be
+#  removed or rewritten.
+text = lambda *args, **kwargs: _text(*args, **kwargs).filter(lambda e: e not in PANDAS_DEFAULT_NULL_VALUES)
+
+
 def arrays_are_identical(expected, result):
     try:
         np.testing.assert_array_equal(expected, result)
@@ -590,7 +597,6 @@ class TestGetDataframe(TestCase):
             expected.columns = expected.columns.str.lower()
 
             result = get_dataframe(src_fp=fp.name, non_na_cols=non_na_cols)
-
             assert_frame_equal(result, expected)
         finally:
             os.remove(fp.name)
