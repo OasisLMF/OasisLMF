d910db89715076949468a1a0ffddcb481c7a719f
sstruzik <stephane.struzik@gmail.com>
2025-03-19 13:27:41 +0000

    simplify code

--- oasislmf/pytools/gulmc/aggregate.py
+++ oasislmf/pytools/gulmc/aggregate.py
@@ -143,16 +143,21 @@ def process_aggregate_vulnerability(aggregate_vulnerability):
 
 
 @nb.njit(cache=True)
-def process_vulnerability_weights(areaperil_dict, aggregate_weights, agg_vuln_to_vuln_id):
+def process_vulnerability_weights(areaperil_dict, vuln_dict, aggregate_weights):
     """Rearrange vulnerability weights from tabular format to a map between (areaperil_id, vulnerability_id) and the vulnerability weight.
+    note: in areaperil_dict, there is no aggregate vulnerability, if one is present in items,
+        the dis-aggregated detailed vuln_in are in areaperil_dict
 
     Args:
         areaperil_dict: dict of areaperil to dict of vuln_id to 0 (0 represent the index but is not set yet)
+        vuln_dict: vuln_dict (Tuple[Dict[int, int]): vulnerability dictionary.
         aggregate_weights (np.array[VulnerabilityWeight]): vulnerability weights table.
+
         agg_vuln_to_vuln_id (dict[int, list[int]]): map of aggregate vulnerability id to list of vulnerability ids.
 
     Returns:
-        dict[AGG_VULN_WEIGHTS_KEY_TYPE, AGG_VULN_WEIGHTS_VAL_TYPE]: map of areaperil_id, vulnerability id to weight.
+        dict[AGG_VULN_WEIGHTS_KEY_TYPE, AGG_VULN_WEIGHTS_VAL_TYPE]: map between the areaperil id and the index where the vulnerability function
+          is stored in `vuln_array` and the vulnerability weight.
     """
     areaperil_vuln_id_to_weight = gen_empty_areaperil_vuln_ids_to_weights()
 
@@ -161,16 +166,14 @@ def process_vulnerability_weights(areaperil_dict, aggregate_weights, agg_vuln_to
 
     for areaperil_id in areaperil_dict:
         for vuln_id in areaperil_dict[areaperil_id]:
-            if vuln_id in agg_vuln_to_vuln_id:
-                for relevant_vuln_id in agg_vuln_to_vuln_id[vuln_id]:
-                    areaperil_vuln_id_to_weight[nb_areaperil_int(areaperil_id), nb_int32(relevant_vuln_id)] = nb_oasis_float(0)
-            else:
-                areaperil_vuln_id_to_weight[nb_areaperil_int(areaperil_id), nb_int32(vuln_id)] = nb_oasis_float(0)
+            areaperil_vuln_id_to_weight[(nb_areaperil_int(areaperil_id), vuln_dict[vuln_id])] = nb_oasis_float(0)
 
     for i in range(len(aggregate_weights)):
         rec = aggregate_weights[i]
-        if (nb_areaperil_int(rec['areaperil_id']), nb_int32(rec['vulnerability_id'])) in areaperil_vuln_id_to_weight:
-            areaperil_vuln_id_to_weight[nb_areaperil_int(rec['areaperil_id']), nb_int32(rec['vulnerability_id'])] = nb_oasis_float(rec['weight'])
+        if rec['vulnerability_id'] in vuln_dict:
+            key = (nb_areaperil_int(rec['areaperil_id']), vuln_dict[rec['vulnerability_id']])
+            if key in areaperil_vuln_id_to_weight:
+                areaperil_vuln_id_to_weight[key] = nb_oasis_float(rec['weight'])
 
     return areaperil_vuln_id_to_weight
 
@@ -195,31 +198,3 @@ def map_agg_vuln_ids_to_agg_vuln_idxs(agg_vulns, agg_vuln_to_vuln_id, vuln_dict)
         agg_vuln_to_vuln_idxs[agg] = List([vuln_dict[vuln] for vuln in agg_vuln_to_vuln_id[agg]])
 
     return agg_vuln_to_vuln_idxs
-
-
-@njit(cache=True)
-def map_areaperil_vuln_id_to_weight_to_areaperil_vuln_idx_to_weight(areaperil_to_vulns, areaperil_vuln_id_to_weight, vuln_dict):
-    """Make map between aggregate vulnerability id and the list of sub-vulnerability ids of which they are composed, where the
-    value of the sub-vulnerability id is the internal pointer (i.e., the index, startinf from 0) to the dense array where they are stored,
-    not the vulnerability id (which starts from 1).
-
-    Args:
-        areaperil_to_vulns (List[int32])
-        areaperil_vuln_id_to_weight (dict[AGG_VULN_WEIGHTS_KEY_TYPE, AGG_VULN_WEIGHTS_VAL_TYPE]): map of areaperil_id, vulnerability id to weight.
-        vuln_dict (Tuple[Dict[int, int]): vulnerability dictionary.
-
-    Returns:
-        dict[AGG_VULN_WEIGHTS_KEY_TYPE, AGG_VULN_WEIGHTS_VAL_TYPE]: map between the areaperil id and the index where the vulnerability function 
-          is stored in `vuln_array` and the vulnerability weight.
-    """
-    areaperil_vuln_idx_to_weight = Dict.empty(AGG_VULN_WEIGHTS_KEY_TYPE, AGG_VULN_WEIGHTS_VAL_TYPE)
-
-    if len(areaperil_vuln_id_to_weight) > 0:
-        for ap in areaperil_to_vulns:
-            for vuln in areaperil_to_vulns[ap]:
-                if tuple((ap, vuln)) in areaperil_vuln_id_to_weight:
-                    # if the weights file contains this definition, add this entry to the map.
-                    # otherwise, the weight of this (areaperil_id, vulnerability_id) is assumed to be zero during loss calculation.
-                    areaperil_vuln_idx_to_weight[tuple((ap, vuln_dict[vuln]))] = areaperil_vuln_id_to_weight[tuple((ap, vuln))]
-
-    return areaperil_vuln_idx_to_weight
