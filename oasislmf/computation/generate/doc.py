b1693514ed0e85eac49e1a5c327a580b3999b1e3
SkylordA <anish.kothikar@oasislmf.org>
2025-04-11 08:25:15 +0100

    adds csv previews for the resources section in md

--- oasislmf/computation/generate/doc.py
+++ oasislmf/computation/generate/doc.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import csv
 from importlib import resources
 import json
 from jsonschema import validate, ValidationError
@@ -124,13 +125,44 @@ class GenerateModelDocumentation(ComputationStep):
             ref_schema = ref_schema.get(part)
         return ref_schema
 
-    def json_to_mdtxt(self, data, properties_schema, full_schema, md, header_level):
+    def json_to_mdtable(self, data, full_schema, md, ref):
+        array_schema = self._resolve_internal_ref(full_schema, ref)
+        array_keys = array_schema["properties"].keys()
+        headers = []
+        for k in array_keys:
+            if "title" in array_schema["properties"][k]:
+                headers.append(array_schema["properties"][k]["title"])
+            else:
+                headers.append(k)
+        rows = []
+        for entry in data:
+            row = []
+            for k in array_keys:
+                v = entry.get(k, "")
+                if isinstance(v, list):
+                    rets = []
+                    for v_ in v:
+                        if isinstance(v_, dict):
+                            v_ = json.dumps(v_, indent=4).replace('\n', '<br>').replace(' ', '&nbsp;')
+                        rets.append(str(v_))
+                    v = ",<br>".join(rets)
+                elif isinstance(v, dict):
+                    pretty_json = json.dumps(v, indent=4).replace('\n', '<br>').replace(' ', '&nbsp;')
+                    v = f"{pretty_json}"
+                else:
+                    v = str(v)
+                row.append(v)
+            rows.append(row)
+        md.add_table(headers, rows)
+
+    def json_to_mdtxt(self, data, properties_schema, full_schema, md, data_path, header_level):
         """Convert json data to markdown text with schemas provided
         Args:
             data (dict): Json data as dictionary
             properties_schema (dict): Data Properties from schema as dictionary
-            full_schema (_type_): Full schema file as dictionary
+            full_schema (dict): Full schema file as dictionary
             md (MarkdownGenerator): MarkdownGenerator class
+            data_path (str | os.PathLike): Path to data folder for any relative file paths
             header_level (int): Header level (number of "#"s to add to headers)
         """
         for key, value in data.items():
@@ -141,51 +173,56 @@ class GenerateModelDocumentation(ComputationStep):
             if "title" in properties_schema[key]:
                 key_title = properties_schema[key]["title"]
 
-            if "type" in properties_schema[key]:
+            if key == "resources":
+                md.add_header(key_title, level=header_level)
+                arr_items = properties_schema[key]["items"]
+                self.json_to_mdtable(value, full_schema, md, arr_items["$ref"])
+                for entry in value:
+                    md.add_header(entry["title"], level=header_level + 1)
+                    entry_fmt = entry["format"]
+                    if entry_fmt == "csv":
+                        if "download_url" not in entry:
+                            md.add_text("No path found to display data")
+                            continue
+                        fp = Path(data_path, entry["download_url"])
+                        if not fp.exists():
+                            md.add_text(f"No file found at {str(fp)}, could not display data")
+                        else:
+                            max_rows = 10
+                            md.add_text(f"First {max_rows} rows displayed only")
+                            with open(fp) as f:
+                                reader = csv.DictReader(f)
+                                headers = reader.fieldnames or []
+                                rows = []
+
+                                for i, row_dict in enumerate(reader):
+                                    if i >= max_rows:
+                                        break
+                                    # Convert row dict to list in header order
+                                    row = [str(row_dict.get(h, "")) for h in headers]
+                                    rows.append(row)
+                            md.add_table(headers, rows)
+                    else:
+                        md.add_text(f"Cannot display preview for {entry_fmt} files")
+            # Markdown code for general nonspecific json data
+            elif "type" in properties_schema[key]:
                 if properties_schema[key]["type"] == "array":
                     md.add_header(key_title, level=header_level)
                     arr_items = properties_schema[key]["items"]
                     if isinstance(arr_items, dict) and "$ref" in arr_items:
-                        array_schema = self._resolve_internal_ref(full_schema, arr_items["$ref"])
-                        array_keys = array_schema["properties"].keys()
-                        headers = []
-                        for k in array_keys:
-                            if "title" in array_schema["properties"][k]:
-                                headers.append(array_schema["properties"][k]["title"])
-                            else:
-                                headers.append(k)
-                        rows = []
-                        for entry in value:
-                            row = []
-                            for k in array_keys:
-                                v = entry.get(k, "")
-                                if isinstance(v, list):
-                                    rets = []
-                                    for v_ in v:
-                                        if isinstance(v_, dict):
-                                            v_ = json.dumps(v_, indent=4).replace('\n', '<br>').replace(' ', '&nbsp;')
-                                        rets.append(str(v_))
-                                    v = ",<br>".join(rets)
-                                elif isinstance(v, dict):
-                                    pretty_json = json.dumps(v, indent=4).replace('\n', '<br>').replace(' ', '&nbsp;')
-                                    v = f"{pretty_json}"
-                                else:
-                                    v = str(v)
-                                row.append(v)
-                            rows.append(row)
-                        md.add_table(headers, rows)
+                        self.json_to_mdtable(value, full_schema, md, arr_items["$ref"])
                     else:
                         md.add_list(value)
                 elif properties_schema[key]["type"] == "object":
                     md.add_header(key_title, level=header_level)
-                    self.json_to_mdtxt(value, properties_schema[key]["properties"], full_schema, md, header_level + 1)
+                    self.json_to_mdtxt(value, properties_schema[key]["properties"], full_schema, md, data_path, header_level + 1)
                 else:
                     md.add_header(key_title, level=header_level)
                     md.add_text(value)
             elif "$ref" in properties_schema[key]:
                 md.add_header(key_title, level=header_level)
                 ref_schema = self._resolve_internal_ref(full_schema, properties_schema[key]["$ref"])
-                self.json_to_mdtxt(value, ref_schema["properties"], full_schema, md, header_level + 1)
+                self.json_to_mdtxt(value, ref_schema["properties"], full_schema, md, data_path, header_level + 1)
             else:
                 md.add_header(key_title, level=header_level)
                 md.add_text(value)
@@ -234,6 +271,7 @@ class GenerateModelDocumentation(ComputationStep):
 
         doc_out_dir = Path(self.doc_out_dir)
         doc_json = Path(self.doc_json)
+        data_path = doc_json.parent
         doc_schema_info = Path(self.doc_schema_info)
         doc_file = Path(doc_out_dir, 'doc.md')
         json_data, schema = self.validate_doc_schema(doc_schema_info, doc_json)
@@ -242,7 +280,7 @@ class GenerateModelDocumentation(ComputationStep):
             md = MarkdownGenerator()
             for i, dataset in enumerate(json_data["datasets"]):
                 md.add_header(dataset["title"], level=2)
-                self.json_to_mdtxt(dataset, schema["properties"], schema, md, 3)
+                self.json_to_mdtxt(dataset, schema["properties"], schema, md, data_path, 3)
                 md.add_text("")
             mdtxt = md.get_markdown()
             toc = self.generate_toc(mdtxt)
