test precision fix

oasislmf/pytools/gulmc/common.py
@@ -33,7 +33,7 @@ items_MC_data_type = nb.from_dtype(np.dtype([item_id[NAME_DTYPE_SLICE],
 VulnCdfLookup = nb.from_dtype(np.dtype([('start', oasis_int),
                                         ('length', oasis_int)]))
 
-coverage_type = nb.from_dtype(np.dtype([tiv[NAME_DTYPE_SLICE],
+coverage_type = nb.from_dtype(np.dtype([('tiv', np.float64),
                                         ('max_items', np.int32),
                                         ('start_items', np.int32),
                                         ('cur_items', np.int32)