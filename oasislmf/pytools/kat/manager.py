b80b769c4312e943d63e5489ce301d90a608c727
SkylordA <anish.kothikar@oasislmf.org>
2026-01-14 10:45:06 +0000

    adds parquet option to katpy cli.py, refactors code a bit

--- oasislmf/pytools/kat/manager.py
+++ oasislmf/pytools/kat/manager.py
@@ -16,58 +16,10 @@ import tempfile
 
 from oasislmf.pytools.common.data import DEFAULT_BUFFER_SIZE, write_ndarray_to_fmt_csv
 from oasislmf.pytools.common.utils.nb_heapq import heap_pop, heap_push, init_heap
-from oasislmf.pytools.elt.data import MELT_dtype, MELT_fmt, MELT_headers, QELT_dtype, QELT_fmt, QELT_headers, SELT_dtype, SELT_fmt, SELT_headers
-from oasislmf.pytools.plt.data import MPLT_dtype, MPLT_fmt, MPLT_headers, QPLT_dtype, QPLT_fmt, QPLT_headers, SPLT_dtype, SPLT_fmt, SPLT_headers
+from oasislmf.pytools.kat.data import KAT_MAP, KAT_QELT, KAT_QPLT, KAT_SELT, KAT_SPLT
 from oasislmf.pytools.utils import redirect_logging
 
 logger = logging.getLogger(__name__)
-
-
-KAT_SELT = 0
-KAT_MELT = 1
-KAT_QELT = 2
-KAT_SPLT = 3
-KAT_MPLT = 4
-KAT_QPLT = 5
-
-KAT_MAP = {
-    KAT_SELT: {
-        "name": "SELT",
-        "headers": SELT_headers,
-        "dtype": SELT_dtype,
-        "fmt": SELT_fmt,
-    },
-    KAT_MELT: {
-        "name": "MELT",
-        "headers": MELT_headers,
-        "dtype": MELT_dtype,
-        "fmt": MELT_fmt,
-    },
-    KAT_QELT: {
-        "name": "QELT",
-        "headers": QELT_headers,
-        "dtype": QELT_dtype,
-        "fmt": QELT_fmt,
-    },
-    KAT_SPLT: {
-        "name": "SPLT",
-        "headers": SPLT_headers,
-        "dtype": SPLT_dtype,
-        "fmt": SPLT_fmt,
-    },
-    KAT_MPLT: {
-        "name": "MPLT",
-        "headers": MPLT_headers,
-        "dtype": MPLT_dtype,
-        "fmt": MPLT_fmt,
-    },
-    KAT_QPLT: {
-        "name": "QPLT",
-        "headers": QPLT_headers,
-        "dtype": QPLT_dtype,
-        "fmt": QPLT_fmt,
-    },
-}
 
 
 def check_file_extensions(file_paths):
