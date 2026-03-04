17c0f27267e8547fa41b6a93c218350042b2b668
sstruzik <stephane.struzik@gmail.com>
2026-03-04 10:12:19 +0000

    fixup mock

--- tests/computation/test_generate_losses.py
+++ tests/computation/test_generate_losses.py
@@ -217,8 +217,14 @@ class TestGenLosses(ComputationChecker):
             self.manager.generate_oasis_losses(**call_args)
             self.assertTrue(mock_post_analysis.called)
 
-    @patch('subprocess.check_output')
-    def test_losses__run_ri__all_outputs__check_bash_script(self, sub_process_run):
+    @patch('subprocess.Popen')
+    def test_losses__run_ri__all_outputs__check_bash_script(self, mock_popen):
+        mock_proc = Mock()
+        mock_proc.pid = 12345
+        mock_proc.communicate.return_value = (b'', None)
+        mock_proc.returncode = 0
+        mock_popen.return_value = mock_proc
+
         summary_type = 'summarypy'
         with self.tmp_dir() as model_run_dir, self.subTest(summary_type):
             self.manager.generate_files(summarypy=summary_type == 'summarypy', **self.args_gen_files_ri)
@@ -236,8 +242,8 @@ class TestGenLosses(ComputationChecker):
                 self.manager.generate_losses(**call_args)
 
             # Check bash script vs reference
-            self.assertTrue(sub_process_run.called)
-            bash_script_path = sub_process_run.call_args.args[0][1]
+            self.assertTrue(mock_popen.called)
+            bash_script_path = mock_popen.call_args.args[0][1]
             result_script = self.read_file(bash_script_path).decode()
             expected_script = self.read_file(ALL_EXPECTED_SCRIPT.format(summary_type)).decode()
             self.assertEqual(expected_script, result_script)
