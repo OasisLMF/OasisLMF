8fa63dcbd605f7f7263a73fb591f0a7dd21ae7ba
SkylordA <anish.kothikar@oasislmf.org>
2026-04-22 15:01:51 +0100

    update coverages bintocsv and csvtobin

--- oasislmf/pytools/converters/csvtobin/utils/coverages.py
+++ oasislmf/pytools/converters/csvtobin/utils/coverages.py
@@ -1,10 +1,15 @@
-from oasislmf.pytools.converters.csvtobin.utils.common import read_csv_as_ndarray
+import numpy as np
+import pandas as pd
+from oasislmf.pytools.common.data import resolve_file
 from oasislmf.pytools.converters.data import TOOL_INFO
 
 
 def coverages_tobin(stack, file_in, file_out, file_type):
-    headers = TOOL_INFO[file_type]["headers"]
     dtype = TOOL_INFO[file_type]["dtype"]
-    data = read_csv_as_ndarray(stack, file_in, headers, dtype)
-
-    data["tiv"].tofile(file_out)
+    tiv_dtype = dtype.fields["tiv"][0]
+    f = resolve_file(file_in, "r", stack)
+    try:
+        tiv = pd.read_csv(f, usecols=["tiv"], dtype={"tiv": tiv_dtype})["tiv"].to_numpy(dtype=tiv_dtype)
+    except pd.errors.EmptyDataError:
+        tiv = np.empty(0, dtype=tiv_dtype)
+    tiv.tofile(file_out)
