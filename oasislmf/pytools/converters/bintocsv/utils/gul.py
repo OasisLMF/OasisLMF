d0eac7572614194917e699478b73a63dad77a7db
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-07-01 15:29:21 +0100

    fix/item_id and event_id type in gul

--- oasislmf/pytools/converters/bintocsv/utils/gul.py
+++ oasislmf/pytools/converters/bintocsv/utils/gul.py
@@ -4,6 +4,7 @@ import numba as nb
 import numpy as np
 from oasislmf.pytools.common.data import (DEFAULT_BUFFER_SIZE, oasis_int, oasis_int_size, loss_pair_dtype, loss_pair_size,
                                           write_ndarray_to_fmt_csv)
+from oasislmf.pytools.common.data import event_id as event_id_dtype, item_id as item_id_dtype
 from oasislmf.pytools.common.event_stream import (
     GUL_STREAM_ID, LOSS_STREAM_ID, EventReader, init_streams_in, mv_read
 )
@@ -11,6 +12,8 @@ from oasislmf.pytools.converters.data import TOOL_INFO
 
 logger = logging.getLogger(__name__)
 
+event_id_dtype = np.dtype(event_id_dtype[1])
+item_id_dtype = np.dtype(item_id_dtype[1])
 
 class GulReader(EventReader):
     def __init__(self, len_sample, data_dtype):
@@ -45,23 +48,17 @@ def read_buffer(byte_mv, cursor, valid_buff, event_id, item_id, data, idxs, stat
     last_event_id = event_id
     idx = idxs[0]
 
-    def _reset_state():
-        state["reading_losses"] = False
-
-    def _update_idxs():
-        idxs[0] = idx
-
     while cursor < valid_buff:
         if not state["reading_losses"]:
             # Read summary header
             if valid_buff - cursor >= 2 * oasis_int_size:
-                event_id_new, cursor = mv_read(byte_mv, cursor, oasis_int, oasis_int_size)
+                event_id_new, cursor = mv_read(byte_mv, cursor, event_id_dtype, event_id_dtype.itemsize)
                 if last_event_id != 0 and event_id_new != last_event_id:
                     # New event, return to process the previous event
-                    _update_idxs()
+                    idxs[0] = idx
                     return cursor - oasis_int_size, last_event_id, item_id, 1
                 event_id = event_id_new
-                item_id, cursor = mv_read(byte_mv, cursor, oasis_int, oasis_int_size)
+                item_id, cursor = mv_read(byte_mv, cursor, item_id_dtype, item_id_dtype.itemsize)
                 state["reading_losses"] = True
             else:
                 break  # Not enough for whole summary header
@@ -78,7 +75,7 @@ def read_buffer(byte_mv, cursor, valid_buff, event_id, item_id, data, idxs, stat
                 sidx = sidx_loss_view[k]["sidx"]
                 if sidx == 0:  # sidx == 0, end of record (loss field is the trailing 0)
                     cursor += (k + 1) * loss_pair_size
-                    _reset_state()
+                    state["reading_losses"] = False
                     break
 
                 data[idx]["event_id"] = event_id
@@ -89,7 +86,7 @@ def read_buffer(byte_mv, cursor, valid_buff, event_id, item_id, data, idxs, stat
                 if idx >= data.shape[0]:
                     # Output array is full
                     cursor += (k + 1) * loss_pair_size
-                    _update_idxs()
+                    idxs[0] = idx
                     return cursor, event_id, item_id, 1
             else:
                 cursor += n_pairs * loss_pair_size
@@ -97,7 +94,7 @@ def read_buffer(byte_mv, cursor, valid_buff, event_id, item_id, data, idxs, stat
             pass  # Should never reach here
 
     # Update the indices
-    _update_idxs()
+    idxs[0] = idx
     return cursor, event_id, item_id, 0
 
 
