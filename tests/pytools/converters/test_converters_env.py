a9c4f6bf829fb8de74307360144208ed54cf02f7
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-07-07 16:44:23 +0100

    run all converters at once

--- tests/pytools/converters/test_converters_env.py
+++ tests/pytools/converters/test_converters_env.py
@@ -7,11 +7,12 @@ from textwrap import dedent
 from tempfile import TemporaryDirectory
 import numpy as np
 import json
+from unittest import TestCase
 
 from oasislmf.pytools.converters.data import TOOL_INFO
 from tests.pytools.converters.test_converters import compare_conversion_outputs, TESTS_ASSETS_DIR
 
-_DTYPE_FILE = "dtype.json"
+_DTYPE_EXT = "dtype.json"
 
 def copy_working_files(source_dir, work_dir, file_in, kwarg_file=None):
     source_dir = Path(source_dir)
@@ -21,18 +22,10 @@ def copy_working_files(source_dir, work_dir, file_in, kwarg_file=None):
         shutil.copyfile(source_dir / kwarg_file, work_dir / kwarg_file)
 
 
-def generate_conversion_script(work_dir, file_in, file_out, file_type,
-                               converter="csvtobin",
-                               kwarg_file=None):
-    # Generate script string
-    out_string = dedent(f"""\
-            from pathlib import Path
-            import json
+def generate_conversion_fragment(work_dir, file_in, file_out, file_type, converter='csvtobin',
+                                 kwarg_file=None):
 
-            from oasislmf.pytools.converters.csvtobin.manager import csvtobin
-            from oasislmf.pytools.converters.bintocsv.manager import bintocsv
-            from oasislmf.pytools.converters.data import TOOL_INFO
-
+    output = dedent(f"""\
             work_dir = Path(\"{work_dir}\")
             kwarg_file = \"{kwarg_file}\"
             if kwarg_file != \"None\":
@@ -49,13 +42,26 @@ def generate_conversion_script(work_dir, file_in, file_out, file_type,
             )
 
             # Serialise dtype
-            with open(work_dir / \"{_DTYPE_FILE}\", "w") as f:
+            with open(work_dir / \"{file_type}_{_DTYPE_EXT}\", "w") as f:
                 json.dump(TOOL_INFO[\"{file_type}\"][\"dtype\"].descr, f)
-            """)
 
+            """)
+    return output
+
+def generate_header_fragment():
+    out_string = dedent("""\
+            from pathlib import Path
+            import json
+
+            from oasislmf.pytools.converters.csvtobin.manager import csvtobin
+            from oasislmf.pytools.converters.bintocsv.manager import bintocsv
+            from oasislmf.pytools.converters.data import TOOL_INFO
+
+            """)
     return out_string
 
-def case_runner(converter, file_type, sub_dir, env_vars, filename=None, kwarg_file=None):
+
+def converter_to_ext(converter):
     if converter == "bintocsv":
         in_ext = ".bin"
         out_ext = ".csv"
@@ -64,149 +70,149 @@ def case_runner(converter, file_type, sub_dir, env_vars, filename=None, kwarg_fi
         out_ext = ".bin"
     else:
         raise RuntimeError(f"Unknown test type {converter}, {file_type}")
+    return in_ext, out_ext
 
-    if filename is None:
-        filename = file_type
 
-    file_in = f"{filename}{in_ext}"
-    file_out = f"{filename}{out_ext}"
+def cases_runner(case_args, tmp_dir, env_vars=None):
+    '''
+    Run multiple cases with single set of adjusted environment variables.
+    '''
+    for case_arg in case_args:
+        case_arg['in_ext'], case_arg['out_ext'] = converter_to_ext(case_arg['converter'])
+
+        if case_arg.get('filename', None) is None:
+            case_arg['filename'] = case_arg['file_type']
+
+        case_arg['file_in'] = f"{case_arg['filename']}{case_arg['in_ext']}"
+        case_arg['file_out'] = f"{case_arg['filename']}_out{case_arg['out_ext']}"
+        case_arg['expected_file_out'] = f"{case_arg['filename']}{case_arg['out_ext']}"
 
     valid_env_vars = ['OASIS_FLOAT', 'OASIS_INT', 'OASIS_AREAPERIL_TYPE']
 
-    with TemporaryDirectory() as tmp_dir:
+    # Copy all necessary input files
+    for case_arg in case_args:
+        file_in = case_arg['file_in']
+        sub_dir = case_arg['sub_dir']
+        kwarg_file = case_arg.get('kwarg_file', None)
         copy_working_files(Path(TESTS_ASSETS_DIR, sub_dir), tmp_dir, file_in,
                            kwarg_file=kwarg_file)
-        script = generate_conversion_script(tmp_dir, file_in, file_out,
-                                            file_type, kwarg_file=kwarg_file,
-                                            converter=converter)
 
-        tmp_path = Path(tmp_dir)
-        script_path = tmp_path / "script.py"
-        with open(script_path, "w") as f:
-            f.write(script)
-
-        # set script env variables
-        env = {**os.environ}
-        for env_key, env_value in env_vars.items():
-            if env_key not in valid_env_vars:
-                continue
-
-            if env_value is None:
-                env.pop(env_key, None)
-            else:
-                env[env_key] = env_value
-
-        result = subprocess.run(
-                [sys.executable, str(script_path)],
-                env = env,
-                capture_output=True,
-                text=True,
-                timeout=300,
+    # Write combined script
+    script = generate_header_fragment()
+    for case_arg in case_args:
+        script += generate_conversion_fragment(
+                            work_dir=tmp_dir,
+                            file_in=case_arg['file_in'],
+                            file_out=case_arg['file_out'],
+                            file_type=case_arg['file_type'],
+                            converter=case_arg['converter'],
+                            kwarg_file=case_arg.get('kwarg_file', None)
                 )
 
-        assert result.returncode == 0, (
-            f"conversion subprocess failed ({result.returncode}):\n"
-            f"STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}"
-        )
+    script_path = Path(tmp_dir) / "script.py"
+    with open(script_path, 'w') as f:
+        f.write(script)
 
-        assert file_out in os.listdir(tmp_dir), f"Output file {file_out} not generated."
+    # Setup environment and run script
+    env = {**os.environ}
+    for env_key, env_value in env_vars.items():
+        if env_key not in valid_env_vars:
+            continue
 
-        with open(tmp_path / _DTYPE_FILE, "r") as f:
-            dtype = np.dtype([tuple(_d) for _d in json.load(f)])
+        if env_value is None:
+            env.pop(env_key, None)
+        else:
+            env[env_key] = env_value
 
-        expected_outfile = Path(TESTS_ASSETS_DIR, sub_dir, file_out)
-        actual_outfile = Path(tmp_dir, file_out)
+    result = subprocess.run(
+            [sys.executable, str(script_path)],
+            env = env,
+            capture_output=True,
+            text=True,
+            timeout=300,
+            )
 
-        compare_conversion_outputs(expected_outfile, actual_outfile, file_type, out_ext,
-                                   dtype=dtype)
+    assert result.returncode == 0, (
+        f"conversion subprocess failed ({result.returncode}):\n"
+        f"STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}"
+    )
 
-def test_coverages():
-    case_runner(converter="csvtobin",
-                file_type="coverages",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    }
-                )
+class MultiConversionTest(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.tmp_dir = self.enterContext(TemporaryDirectory())
 
-    case_runner(converter="bintocsv",
-                file_type="coverages",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    },
-                )
+        self.case_args = [
+            dict(converter="csvtobin", file_type="coverages",
+                 sub_dir="envdtype",),
+            dict(converter="bintocsv", file_type="coverages",
+                 sub_dir="envdtype",),
 
-def test_damagebin():
-    case_runner(converter="csvtobin",
-                file_type="damagebin",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    },
-                kwarg_file="damagebin_args.json"
-                )
+            dict(converter="csvtobin", file_type="damagebin",
+                 sub_dir="envdtype", kwarg_file="damagebin_args.json"),
+            dict(converter="bintocsv", file_type="damagebin",
+                 sub_dir="envdtype",),
 
-    case_runner(converter="bintocsv",
-                file_type="damagebin",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    },
-                )
+            dict(converter="csvtobin", file_type="items", sub_dir="envdtype",),
+            dict(converter="bintocsv", file_type="items", sub_dir="envdtype",),
 
-def test_items():
-    case_runner(converter="csvtobin",
-                file_type="items",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_AREAPERIL_TYPE": "u8"
-                    }
-                )
+            # only test vuln noidx route
+            dict(converter="csvtobin", file_type="vulnerability",
+                 sub_dir="envdtype",
+                 kwarg_file="vulnerability_csvtobin_args.json",),
+            dict(converter="bintocsv", file_type="vulnerability",
+                 sub_dir="envdtype",
+                 kwarg_file="vulnerability_bintocsv_args.json",
+                ),
 
-    case_runner(converter="bintocsv",
-                file_type="items",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_AREAPERIL_TYPE": "u8"
-                    },
-                )
-
-def test_vulnerability():
-    # only testing noidx route
-    case_runner(converter="csvtobin",
-                file_type="vulnerability",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    },
-                kwarg_file="vulnerability_csvtobin_args.json",
-                )
-
-    case_runner(converter="bintocsv",
-                file_type="vulnerability",
-                sub_dir="envdtype",
-                env_vars={
-                    "OASIS_FLOAT": "f8"
-                    },
-                kwarg_file="vulnerability_bintocsv_args.json",
-                )
-
-def test_weights():
-    case_runner(converter="csvtobin",
+            dict(converter="csvtobin",
                 file_type="weights",
                 sub_dir="envdtype",
-                env_vars={
-                    "OASIS_AREAPERIL_TYPE": "u8",
-                    "OASIS_FLOAT": "f8"
-                    },
-                )
-
-    case_runner(converter="bintocsv",
+                ),
+            dict(converter="bintocsv",
                 file_type="weights",
                 sub_dir="envdtype",
-                env_vars={
-                    "OASIS_AREAPERIL_TYPE": "u8",
-                    "OASIS_FLOAT": "f8"
-                    },
-                )
+                ),
+            ]
+
+        env_args = { "OASIS_FLOAT": "f8", "OASIS_AREAPERIL_TYPE": "u8" }
+
+        cases_runner(self.case_args, tmp_dir=self.tmp_dir, env_vars=env_args)
+
+
+    def _run_general_case(self, file_type):
+        case_args = [ca for ca in self.case_args if ca['file_type'] == file_type]
+
+        for args in case_args:
+            file_out = args['file_out']
+            expected_file_out = args['expected_file_out']
+            out_ext = args['out_ext']
+            sub_dir = args['sub_dir']
+            assert file_out in os.listdir(self.tmp_dir), f"Output file {file_out} not generated."
+
+            with open(Path(self.tmp_dir, f"{file_type}_{_DTYPE_EXT}"), "r") as f:
+                dtype = np.dtype([tuple(_d) for _d in json.load(f)])
+
+            expected_outfile = Path(TESTS_ASSETS_DIR, sub_dir, expected_file_out)
+            actual_outfile = Path(self.tmp_dir, file_out)
+
+            compare_conversion_outputs(expected_outfile, actual_outfile, file_type, out_ext,
+                                       dtype=dtype)
+
+
+    def test_coverages(self):
+        self._run_general_case(file_type="coverages")
+
+    def test_damagebin(self):
+        self._run_general_case(file_type="damagebin")
+
+    def test_items(self):
+        self._run_general_case(file_type="items")
+
+    def test_vulnerability(self):
+        self._run_general_case(file_type="vulnerability")
+
+    def test_weights(self):
+        self._run_general_case(file_type="weights")
+
+
