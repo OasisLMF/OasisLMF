a632d8facdaf7bae49509c7b66ca5841eb1f058a
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-07-14 19:06:14 +0100

    flake8

--- oasislmf/pytools/converters/csvtobin/utils/fm.py
+++ oasislmf/pytools/converters/csvtobin/utils/fm.py
@@ -1,10 +1,10 @@
 import numba as nb
 import numpy as np
 from oasislmf.pytools.common.data import DEFAULT_BUFFER_SIZE
-from oasislmf.pytools.common.event_stream import mv_write, mv_write_item_header, mv_write_sidx_loss
+from oasislmf.pytools.common.event_stream import mv_write_item_header, mv_write_sidx_loss
 from oasislmf.pytools.converters.csvtobin.utils.common import iter_csv_as_ndarray
 from oasislmf.pytools.converters.data import TOOL_INFO
-from oasislmf.pytools.common.data import loss_pair_dtype, item_header_dtype, oasis_int, def_to_type_and_size_str, oasis_float, oasis_int_size, oasis_float_size
+from oasislmf.pytools.common.data import loss_pair_dtype, item_header_dtype, def_to_type_and_size_str
 
 # Worst case: every input row opens a new group (2 header + 2 data + 2 termination = 6 int32s)
 _CHUNK_OUT_SIZE = DEFAULT_BUFFER_SIZE * (item_header_dtype.itemsize + loss_pair_dtype.itemsize + oasis_int.itemsize * 2)
